f7fde110bb8884fae70d9ad466b901be22498a3b
Silvano Seva <silseva@fastwebnet.it>
2020-11-13 14:31:21 +0100

    Tone generator for CTCSS and user interface 'beep' on MD380

--- platform/drivers/tones/toneGenerator_MDxx380.h
+++ platform/drivers/tones/toneGenerator_MDxx380.h
@@ -1,6 +1,7 @@
 /***************************************************************************
  *   Copyright (C) 2020 by Federico Amedeo Izzo IU2NUO,                    *
  *                         Niccolò Izzo IU2KIN                             *
+ *                         Frederik Saraci IU2NRO                          *
  *                         Silvano Seva IU2KWO                             *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -17,62 +18,65 @@
  *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
  ***************************************************************************/
 
-#ifndef HWCONFIG_H
-#define HWCONFIG_H
+#ifndef TONE_GENERATOR_H
+#define TONE_GENERATOR_H
 
-#include <stm32f4xx.h>
+#include <stdint.h>
 
-#define PLATFORM_MD380
-
-/* Screen dimensions */
-#define SCREEN_WIDTH 160
-#define SCREEN_HEIGHT 128
-
-/* Display */
-#define LCD_D0  GPIOD,14
-#define LCD_D1  GPIOD,15
-#define LCD_D2  GPIOD,0
-#define LCD_D3  GPIOD,1
-#define LCD_D4  GPIOE,7
-#define LCD_D5  GPIOE,8
-#define LCD_D6  GPIOE,9
-#define LCD_D7  GPIOE,10
-#define LCD_WR  GPIOD,5
-#define LCD_RD  GPIOD,4
-#define LCD_CS  GPIOD,6
-#define LCD_RS  GPIOD,12
-#define LCD_RST GPIOD,13
-#define LCD_BKLIGHT GPIOC,6
-
-/* Signalling LEDs */
-#define GREEN_LED  GPIOE,0
-#define RED_LED    GPIOE,1
-
-/* Analog inputs */
-#define AIN_VOLUME GPIOA,0
-#define AIN_VBAT   GPIOA,1
-#define AIN_MIC    GPIOA,3
-#define AIN_RSSI   GPIOB,0
-
-/* Channel selection rotary encoder */
-#define CH_SELECTOR_0 GPIOE,14
-#define CH_SELECTOR_1 GPIOE,15
-#define CH_SELECTOR_2 GPIOB,10
-#define CH_SELECTOR_3 GPIOB,11
-
-/* Push-to-talk switch */
-#define PTT_SW GPIOE,11
-
-/*
- * Keyboard. Here we define only rows, since coloumn lines are the same as
- * LCD_Dx. See also: https://www.qsl.net/dl4yhf/RT3/md380_hw.html#keyboard
+/**
+ * Tone generator for MDxx380 family, used for both CTCSS tones and user
+ * interface "beeps".
+ * This driver uses TIM3 of STM32F405 mcu in PWM mode to generate sinewaves
+ * using a precomputed sine table.
  */
-#define KB_ROW1 GPIOA,6 /* K1 */
-#define KB_ROW2 GPIOD,2 /* K2 */
-#define KB_ROW3 GPIOD,3 /* K3 */
 
-/* Tone generator  */
-#define CTCSS_OUT GPIOC,7   /* System "beep" */
-#define BEEP_OUT  GPIOC,8   /* CTCSS tone    */
+/**
+ * Initialise tone generator.
+ */
+void toneGen_init();
 
-#endif
+/**
+ * Terminate tone generator.
+ */
+void toneGen_shutdown();
+
+/**
+ * Set frequency for CTCSS tone generation.
+ * @param toneFreq: CTCSS tone frequency.
+ */
+void toneGen_setToneFreq(float toneFreq);
+
+/**
+ * Activate generation of CTCSS tone.
+ */
+void toneGen_toneOn();
+
+/**
+ * Terminate generation of CTCSS tone.
+ */
+void toneGen_toneOff();
+
+/**
+ * Set frequency for user interface "beep".
+ * @param beepFreq: frequency of "beep" tone.
+ */
+void toneGen_setBeepFreq(float beepFreq);
+
+/**
+ * Activate generation of "beep" tone.
+ */
+void toneGen_beepOn();
+
+/**
+ * Terminate generation of "beep" tone.
+ */
+void toneGen_beepOff();
+
+/**
+ * Activate generation of "beep" tone with automatic termination after a given
+ * amount of time.
+ * @param duration: duration of "beep" tone, in milliseconds.
+ */
+void toneGen_timedBeep(uint16_t duration);
+
+#endif /* TONE_GENERATOR_H */
