d3796971dfd4faacdda8801af42cb025d9df58f8
Federico Amedeo Izzo <federico@izzo.pro>
2021-01-24 16:29:48 +0100

    NVMEM: Fix compiler warning on linux

--- platform/drivers/NVM/nvmem_linux.c
+++ platform/drivers/NVM/nvmem_linux.c
@@ -18,6 +18,7 @@
  *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
  ***************************************************************************/
 
+#include <stdio.h>
 #include <interfaces/nvmem.h>
 
 // Simulate CPS with 8 channels
