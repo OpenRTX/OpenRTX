aa3680ee462af69712ca865aee525270dabe9b73
Silvano Seva <silseva@fastwebnet.it>
2021-03-09 21:05:53 +0100

    Moving to miosix kernel: boot files for MK22FN512-based targets

--- platform/mcu/MK22FN512xxx12/boot/startup.cpp
+++ platform/mcu/MK22FN512xxx12/boot/startup.cpp
@@ -1,6 +1,9 @@
 /***************************************************************************
- *   Copyright (C) 2020 by Federico Izzo IU2NUO, Niccolò Izzo IU2KIN and   *
- *                         Silvano Seva IU2KWO                             *
+ *   Copyright (C) 2021 by Federico Amedeo Izzo IU2NUO,                    *
+ *                         Niccolò Izzo IU2KIN                             *
+ *                         Frederik Saraci IU2NRO                          *
+ *                         Silvano Seva IU2KWO,                            *
+ *                         Federico Terraneo                               *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -15,25 +18,41 @@
  *   You should have received a copy of the GNU General Public License     *
  *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
  ***************************************************************************/
- 
 
-#include <stdio.h>
+/*
+ * startup.cpp
+ * STM32 C++ startup.
+ * NOTE: for stm32f4 devices ONLY.
+ * Supports interrupt handlers in C++ without extern "C"
+ * Developed by Terraneo Federico, based on ST startup code.
+ * Additionally modified to boot Miosix.
+ */
+
+#include "interfaces/arch_registers.h"
+#include "kernel/stage_2_boot.h"
+#include "core/interrupts.h" //For the unexpected interrupt call
 #include <string.h>
-#include <usb_vcom.h>
-#include <MK22F51212.h>
-#include <system_MK22F51212.h>
 
-///< Entry point for system bootstrap after initial configurations.
-void systemBootstrap();
-
-void Reset_Handler() __attribute__((__interrupt__, noreturn));
-void Reset_Handler()
+/**
+ * Called by Reset_Handler, performs initialization and calls main.
+ * Never returns.
+ */
+void program_startup() __attribute__((noreturn));
+void program_startup()
 {
-    // Disable interrupts at startup, as the RTOS requires this.
+    //Cortex M3 core appears to get out of reset with interrupts already enabled
     __disable_irq();
 
-    // Call CMSIS init function, it's safe to do it here.
-    // This function initialises VTOR.
+    //SystemInit() is called *before* initializing .data and zeroing .bss
+    //Despite all startup files provided by ST do the opposite, there are three
+    //good reasons to do so:
+    //First, the CMSIS specifications say that SystemInit() must not access
+    //global variables, so it is actually possible to call it before
+    //Second, when running Miosix with the xram linker scripts .data and .bss
+    //are placed in the external RAM, so we *must* call SystemInit(), which
+    //enables xram, before touching .data and .bss
+    //Third, this is a performance improvement since the loops that initialize
+    //.data and zeros .bss now run with the CPU at full speed instead of 8MHz
     SystemInit();
 
     // Initialise clock tree for full speed run, since SystemInit by NXP does not.
@@ -72,49 +91,66 @@ void Reset_Handler()
     extern unsigned char _bss_start asm("_bss_start");
     extern unsigned char _bss_end asm("_bss_end");
 
-    // Initialize .data section, clear .bss section
+    //Initialize .data section, clear .bss section
     unsigned char *etext=&_etext;
     unsigned char *data=&_data;
     unsigned char *edata=&_edata;
     unsigned char *bss_start=&_bss_start;
     unsigned char *bss_end=&_bss_end;
-
     memcpy(data, etext, edata-data);
     memset(bss_start, 0, bss_end-bss_start);
 
-    SIM->SCGC5 |= 0x3E00;   // Enable GPIO clock
+    //Move on to stage 2
+    _init();
 
-    // Enable virtual com port (for stdin, stdout and stderr redirection)
-    vcom_init();
-
-    // Set no buffer for stdin, required to make scanf, getchar, ... working
-    // correctly
-    setvbuf(stdin, NULL, _IONBF, 0);
-
-
-    // Jump to application code
-    systemBootstrap();
-
-    // If main returns loop indefinitely
+    //If main returns, reboot
+    NVIC_SystemReset();
     for(;;) ;
 }
 
-void Default_Handler()
+/**
+ * Reset handler, called by hardware immediately after reset
+ */
+void Reset_Handler() __attribute__((__interrupt__, noreturn));
+void Reset_Handler()
 {
-    // default handler does nothing
+    /*
+     * Initialize process stack and switch to it.
+     * This is required for booting Miosix, a small portion of the top of the
+     * heap area will be used as stack until the first thread starts. After,
+     * this stack will be abandoned and the process stack will point to the
+     * current thread's stack.
+     */
+    asm volatile("ldr r0,  =_heap_end          \n\t"
+                 "msr psp, r0                  \n\t"
+                 "movw r0, #2                  \n\n" //Privileged, process stack
+                 "msr control, r0              \n\t"
+                 "isb                          \n\t":::"r0");
+
+    program_startup();
 }
 
-void __attribute__((weak)) NMI_Handler();
-void __attribute__((weak)) HardFault_Handler();
-void __attribute__((weak)) MemManage_Handler();
-void __attribute__((weak)) BusFault_Handler();
-void __attribute__((weak)) UsageFault_Handler();
-void __attribute__((weak)) SVC_Handler();
-void __attribute__((weak)) DebugMon_Handler();
+/**
+ * All unused interrupts call this function.
+ */
+extern "C" void Default_Handler()
+{
+    unexpectedInterrupt();
+}
 
-void __attribute__((weak)) OS_CPU_PendSVHandler();  // uC/OS-III naming convention
-void __attribute__((weak)) OS_CPU_SysTickHandler();
+//System handlers
+void /*__attribute__((weak))*/ Reset_Handler();     //These interrupts are not
+void /*__attribute__((weak))*/ NMI_Handler();       //weak because they are
+void /*__attribute__((weak))*/ HardFault_Handler(); //surely defined by Miosix
+void /*__attribute__((weak))*/ MemManage_Handler();
+void /*__attribute__((weak))*/ BusFault_Handler();
+void /*__attribute__((weak))*/ UsageFault_Handler();
+void /*__attribute__((weak))*/ SVC_Handler();
+void /*__attribute__((weak))*/ DebugMon_Handler();
+void /*__attribute__((weak))*/ PendSV_Handler();
+void /*__attribute__((weak))*/ SysTick_Handler();
 
+//Interrupt handlers
 void __attribute__((weak)) DMA0_IRQHandler();
 void __attribute__((weak)) DMA1_IRQHandler();
 void __attribute__((weak)) DMA2_IRQHandler();
@@ -191,14 +227,14 @@ void __attribute__((weak)) DAC1_IRQHandler();
 void __attribute__((weak)) ADC1_IRQHandler();
 
 //Stack top, defined in the linker script
-extern char _stack_top asm("_stack_top");
+extern char _main_stack_top asm("_main_stack_top");
 
 //Interrupt vectors, must be placed @ address 0x00000000
 //The extern declaration is required otherwise g++ optimizes it out
 extern void (* const __Vectors[])();
 void (* const __Vectors[])() __attribute__ ((section(".isr_vector"))) =
 {
-    (void (*)())(&_stack_top),
+    reinterpret_cast<void (*)()>(&_main_stack_top),/* Stack pointer*/
     Reset_Handler,
     NMI_Handler,
     HardFault_Handler,
@@ -212,8 +248,8 @@ void (* const __Vectors[])() __attribute__ ((section(".isr_vector"))) =
     SVC_Handler,
     DebugMon_Handler,
     0,
-    OS_CPU_PendSVHandler,
-    OS_CPU_SysTickHandler,
+    PendSV_Handler,
+    SysTick_Handler,
 
     DMA0_IRQHandler,            /* DMA Channel 0 Transfer Complete*/
     DMA1_IRQHandler,            /* DMA Channel 1 Transfer Complete*/
@@ -290,17 +326,6 @@ void (* const __Vectors[])() __attribute__ ((section(".isr_vector"))) =
     DAC1_IRQHandler,            /* DAC1 interrupt*/
     ADC1_IRQHandler             /* ADC1 interrupt*/
 };
-
-#pragma weak NMI_Handler= Default_Handler
-#pragma weak HardFault_Handler= Default_Handler
-#pragma weak MemManage_Handler= Default_Handler
-#pragma weak BusFault_Handler= Default_Handler
-#pragma weak UsageFault_Handler= Default_Handler
-#pragma weak SVC_Handler= Default_Handler
-#pragma weak DebugMon_Handler= Default_Handler
-#pragma weak PendSV_Handler= Default_Handler
-#pragma weak SysTick_Handler= Default_Handler
-#pragma weak WWDG_IRQHandler= Default_Handler
 
 #pragma weak DMA0_IRQHandler = Default_Handler
 #pragma weak DMA1_IRQHandler = Default_Handler
