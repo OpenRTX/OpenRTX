a8b838bd47e14dde9c44e41e4f850ccd7703a488
Federico Amedeo Izzo <federico@izzo.pro>
2021-07-08 20:24:27 +0200

    On Linux target emulate PTT button with P key

--- platform/targets/linux/platform.c
+++ platform/targets/linux/platform.c
@@ -19,6 +19,7 @@
 #include <interfaces/gpio.h>
 #include <stdio.h>
 #include "emulator.h"
+#include <SDL2/SDL.h>
 
 hwInfo_t hwInfo;
 
@@ -76,7 +77,13 @@ int8_t platform_getChSelector()
 
 bool platform_getPttStatus()
 {
-    return Radio_State.PttStatus;
+    // Read P key status from SDL
+    SDL_PumpEvents();
+    const uint8_t *state = SDL_GetKeyboardState(NULL);
+    if (state[SDL_SCANCODE_P])
+        return true;
+    else
+        return false;
 }
 
 bool platform_pwrButtonStatus()
