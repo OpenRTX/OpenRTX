8baacaca0b89a20f2053a3ce82057546bc8f0311
Silvano Seva <silseva@fastwebnet.it>
2021-09-19 22:26:35 +0200

    Very preliminary support for Module17, providing only the files and configurations essential for a minimal bootstrap of the OpenRTX firmware

---
+++ platform/drivers/baseband/radio_Mod17.cpp
@@ -0,0 +1,76 @@
+/***************************************************************************
+ *   Copyright (C) 2021 by Federico Amedeo Izzo IU2NUO,                    *
+ *                         Niccolò Izzo IU2KIN                             *
+ *                         Frederik Saraci IU2NRO                          *
+ *                         Silvano Seva IU2KWO                             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
+ ***************************************************************************/
+
+#include <interfaces/platform.h>
+#include <interfaces/radio.h>
+
+void radio_init(const rtxStatus_t *rtxState)
+{
+    (void) rtxState;
+}
+
+void radio_terminate()
+{
+}
+
+void radio_tuneVcxo(const int16_t vhfOffset, const int16_t uhfOffset)
+{
+    (void) vhfOffset;
+    (void) uhfOffset;
+}
+
+void radio_setOpmode(const enum opmode mode)
+{
+    (void) mode;
+}
+
+bool radio_checkRxDigitalSquelch()
+{
+    return false;
+}
+
+void radio_enableRx()
+{
+}
+
+void radio_enableTx()
+{
+
+}
+
+void radio_disableRtx()
+{
+
+}
+
+void radio_updateConfiguration()
+{
+
+}
+
+float radio_getRssi()
+{
+    return -123.0f;
+}
+
+enum opstatus radio_getStatus()
+{
+    return OFF;
+}
---
+++ platform/targets/Module17/platform.c
@@ -0,0 +1,113 @@
+/***************************************************************************
+ *   Copyright (C) 2021 by Federico Amedeo Izzo IU2NUO,                    *
+ *                         Niccolò Izzo IU2KIN,                            *
+ *                         Frederik Saraci IU2NRO,                         *
+ *                         Silvano Seva IU2KWO                             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
+ ***************************************************************************/
+
+#include <interfaces/gpio.h>
+#include <interfaces/nvmem.h>
+#include <interfaces/platform.h>
+#include <hwconfig.h>
+#include <string.h>
+#include <ADC1_MDx.h>
+#include <backlight.h>
+#include <calibInfo_MDx.h>
+#include <toneGenerator_MDx.h>
+#include <interfaces/rtc.h>
+#include <interfaces/audio.h>
+
+void platform_init()
+{
+    /* Configure GPIOs */
+    gpio_setMode(PTT_LED,  OUTPUT);
+    gpio_setMode(SYNC_LED, OUTPUT);
+    gpio_setMode(ERR_LED,  OUTPUT);
+}
+
+void platform_terminate()
+{
+    /* Shut down LEDs */
+    gpio_clearPin(PTT_LED);
+    gpio_clearPin(SYNC_LED);
+    gpio_clearPin(ERR_LED);
+}
+
+uint16_t platform_getVbat()
+{
+    return 0;
+}
+
+uint8_t platform_getMicLevel()
+{
+    return 0;
+}
+
+uint8_t platform_getVolumeLevel()
+{
+    return 0;
+}
+
+int8_t platform_getChSelector()
+{
+    return 0;
+}
+
+bool platform_getPttStatus()
+{
+    return false;
+}
+
+bool platform_pwrButtonStatus()
+{
+    return false;
+}
+
+void platform_ledOn(led_t led)
+{
+    (void) led;
+}
+
+void platform_ledOff(led_t led)
+{
+    (void) led;
+}
+
+void platform_beepStart(uint16_t freq)
+{
+    /* TODO */
+    (void) freq;
+}
+
+void platform_beepStop()
+{
+    /* TODO */
+}
+
+const void *platform_getCalibrationData()
+{
+    return NULL;
+}
+
+const hwInfo_t *platform_getHwInfo()
+{
+    return NULL;
+}
+
+void platform_setBacklightLevel(uint8_t level)
+{
+    (void) level;
+}
