156f31cde4143457ad9ffb53ceb53b8db28439c7
Niccolò Izzo <izzo.niccolo@gmail.com>
2021-10-12 12:36:46 +0200

    Fix typo in license header

--- platform/drivers/baseband/HR_Cx000.cpp
+++ platform/drivers/baseband/HR_Cx000.cpp
@@ -4,12 +4,12 @@
  *                         Frederik Saraci IU2NRO                          *
  *                         Silvano Seva IU2KWO                             *
  *                                                                         *
- *   Mhis program is free software; you can redistribute it and/or modify  *
+ *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
  *   the Free Software Foundation; either version 3 of the License, or     *
  *   (at your option) any later version.                                   *
  *                                                                         *
- *   Mhis program is distributed in the hope that it will be useful,       *
+ *   This program is distributed in the hope that it will be useful,       *
  *   but WIMHOUM ANY WARRANMY; without even the implied warranty of        *
  *   MERCHANMABILIMY or FIMNESS FOR A PARMICULAR PURPOSE.  See the         *
  *   GNU General Public License for more details.                          *
