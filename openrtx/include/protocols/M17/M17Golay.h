ad6f49f941109dc9d3beab0e2e94c95b2fae00c4
Silvano Seva <silseva@fastwebnet.it>
2021-10-25 22:18:07 +0200

    Implementation of Golay(24,12) decoder, reimplemented Golay(24,12) encoder using generator matrix, unit test for Golay(24,12) encoding and decoding routines

--- openrtx/include/protocols/M17/M17Golay.h
+++ openrtx/include/protocols/M17/M17Golay.h
@@ -1,10 +1,10 @@
 /***************************************************************************
  *   Copyright (C) 2021 by Federico Amedeo Izzo IU2NUO,                    *
  *                         Niccolò Izzo IU2KIN                             *
+ *                         Wojciech Kaczmarski SP5WWP                      *
  *                         Frederik Saraci IU2NRO                          *
  *                         Silvano Seva IU2KWO                             *
  *                                                                         *
- *   Adapted from original code written by Rob Riggs, Mobilinkd LLC        *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -29,9 +29,115 @@
 
 #include <cstdint>
 
+namespace Golay24
+{
+
 /**
- * Compute the Golay(24,12) codeword of a given block of data using the M17
- * Golay polynomial.
+ * Data encoding matrix for Golay(24,12) code with generator polynomial 0xC75;
+ */
+static const uint16_t encode_matrix[12] =
+{
+    0x8eb, 0x93e, 0xa97, 0xdc6, 0x367, 0x6cd,
+    0xd99, 0x3da, 0x7b4, 0xf68, 0x63b, 0xc75
+};
+
+
+/**
+ * Data decoding matrix for Golay(24,12) code with generator polynomial 0xC75;
+ */
+static const uint16_t decode_matrix[12] =
+{
+    0xc75, 0x49f, 0x93e, 0x6e3, 0xdc6, 0xf13,
+    0xab9, 0x1ed, 0x3da, 0x7b4, 0xf68, 0xa4f
+};
+
+
+/**
+ * Function computing the Golay(24,12) checksum of a given 12-bit data block.
+ *
+ * @param value: input data.
+ * @return Golay(24,12) checksum.
+ */
+static uint16_t calcChecksum(const uint16_t& value)
+{
+    uint16_t checksum = 0;
+
+    for(uint8_t i = 0; i < 12; i++)
+    {
+        if(value & (1 << i))
+        {
+            checksum ^= encode_matrix[i];
+        }
+    }
+
+    return checksum;
+}
+
+
+/**
+ * Detect and correct errors in a Golay(24,12) codeword.
+ *
+ * @param codeword: input codeword.
+ * @return bitmask corresponding to detected bit errors in the codeword, or
+ * 0xFFFFFFFF if bit errors are unrecoverable.
+ */
+static uint32_t detectErrors(const uint32_t& codeword)
+{
+    uint16_t data   = codeword >> 12;
+    uint16_t parity = codeword & 0xFFF;
+
+    uint16_t syndrome = parity ^ calcChecksum(data);
+
+    if(__builtin_popcount(syndrome) <= 3)
+    {
+        return syndrome;
+    }
+
+    for(uint8_t i = 0; i<12; i++)
+    {
+        uint16_t e = 1 << i;
+        uint16_t coded_error = encode_matrix[i];
+
+        if(__builtin_popcount(syndrome^coded_error) <= 2)
+        {
+            return (e << 12) | (syndrome ^ coded_error);
+        }
+    }
+
+
+    uint16_t inv_syndrome = 0;
+    for(uint8_t i = 0; i < 12; i++)
+    {
+        if(syndrome & (1 << i))
+        {
+            inv_syndrome ^= decode_matrix[i];
+        }
+    }
+
+    if(__builtin_popcount(inv_syndrome) <= 3)
+    {
+        return inv_syndrome << 12;
+    }
+
+    for(uint8_t i = 0; i < 12; i++ )
+    {
+        uint16_t e = 1 << i;
+        uint16_t coding_error = decode_matrix[i];
+        if(__builtin_popcount(inv_syndrome ^ coding_error) <= 2 )
+        {
+            return ((inv_syndrome ^ coding_error) << 12) | e;
+        }
+    }
+
+    return 0xFFFFFFFF;
+}
+
+}   // namespace Golay24
+
+
+/**
+ * Compute the Golay(24,12) codeword of a given block of data using the
+ * generator polynomial 0xC75.
  * Result is composed as follows:
  *
  * +--------+----------+--------+
@@ -43,21 +149,25 @@
  * \param data: input data, upper four bits are discarded.
  * \return resulting 24 bit codeword.
  */
-static uint32_t golay_encode24(const uint16_t& data)
+static inline uint32_t golay24_encode(const uint16_t& data)
 {
-    // Compute [23,12] Golay codeword
-    uint32_t codeword = data & 0x0FFFF;
-    for(size_t i = 0; i < 12; i++)
-    {
-        if(codeword & 0x01) codeword ^= 0x0C75;
-        codeword >>= 1;
-    }
+    return (data << 12) | Golay24::calcChecksum(data);
+}
 
-    // Append data to codeword, result is checkbits(11) | data(12)
-    codeword |= (data << 11);
 
-    // Compute parity and append it to codeword
-    return (codeword << 1) | (__builtin_popcount(codeword) & 0x01);
+/**
+ * Decode a Golay(24,12) codeword, correcting eventual bit errors. In case the
+ * bit errors are not correctable, the function returns 0xFFFF, a value outside
+ * the range allowed for the 12-bit input data required by Golay coding.
+ *
+ * \param codeword: input Golay(24,12) codeword.
+ * \return original data block or 0xFFFF in case of unrecoverable errors.
+ */
+static inline uint16_t golay24_decode(const uint32_t& codeword)
+{
+    uint32_t errors = Golay24::detectErrors(codeword);
+    if(errors == 0xFFFFFFFF) return 0xFFFF;
+    return ((codeword ^ errors) >> 12) & 0x0FFF;
 }
 
 #endif /* M17_GOLAY_H */
