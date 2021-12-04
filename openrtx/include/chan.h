e2baf5a833b8dcbf7722f866fc20731fe9a8433a
Alessio Caiazza <nolith@abisso.org>
2021-12-04 19:17:00 +0100

    Document functions with Doxygen format

--- openrtx/include/chan.h
+++ openrtx/include/chan.h
@@ -20,7 +20,7 @@
 #include <pthread.h>
 #include <stdbool.h>
 
-/*
+/**
  * chan_t is an unbuffered synchronization channel.
  * Both reader and writer are blocked untill the data is exchanged.
  */
@@ -39,12 +39,60 @@ typedef struct chan_t
 }
 chan_t;
 
+/**
+ * This function initializes a channel.
+ *
+ * @param c: the cannel to initialize.
+ */
 void chan_init(chan_t *c);
+
+/**
+ * This function writes data into a channel.
+ * This is a synchronous write and the function blocks until a read happens.
+ *
+ * @param c: the channel.
+ * @param data: the data to publish on the channel.
+ */
 void chan_send(chan_t *c, void *data);
+
+/**
+ * This function reads data from a channel.
+ * This is a synchronous read and the function blocks until a write happens.
+ *
+ * @param c: the channel.
+ * @param data: the read data will be assigned to this pointer.
+ */
 void chan_recv(chan_t *c, void **data);
+
+/**
+ * This function check if the channel has a writer waiting with data.
+ *
+ * @param c: the channel.
+ * @return true if a writer is waiting on the channel.
+ */
 bool chan_can_recv(chan_t *c);
+
+/**
+ * This function check if the channel has a reader waiting for data.
+ *
+ * @param c: the channel.
+ * @return true if a reader is waiting on the channel.
+ */
 bool chan_can_send(chan_t *c);
+
+/**
+ * This function closes a channel.
+ * When a channel is closed, it is no longer possible to read or write from it.
+ *
+ * @param c: the channel.
+ */
 void chan_close(chan_t *c);
+
+/**
+ * Destructor function to de-allocate a channel.
+ *
+ * @param c: the channel.
+ */
 void chan_terminate(chan_t *c);
 
 #endif
