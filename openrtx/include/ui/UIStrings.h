a21ee05f0b2c8e4e8a4bb74eb6870d869dec39cf
vk7js <58905135+vk7js@users.noreply.github.com>
2022-05-04 21:31:39 +1000

    Woops, I forgot to check in UIStrings.h

---
+++ openrtx/include/ui/UIStrings.h
@@ -0,0 +1,41 @@
+/***************************************************************************
+ *   Copyright (C) 2020 by Federico Amedeo Izzo IU2NUO,                    *
+ *                         Niccolò Izzo IU2KIN                             *
+ *                         Frederik Saraci IU2NRO                          *
+ *                         Silvano Seva IU2KWO                             *
+ *                         Joseph Stephen VK7JS                            * 
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
+ ***************************************************************************/
+/*
+This string table's order must not be altered as voice prompts will be indexed in the same order as these strings.
+*/
+#define NUM_LANGUAGES 1
+
+typedef struct
+{
+	const char* languageName;
+	const char* off;
+	const char* on;
+} stringsTable_t;
+
+const stringsTable_t englishStrings =
+{
+	.languageName="English",
+	.off = "OFF",
+	.on="ON",
+};
+
+const stringsTable_t languages[NUM_LANGUAGES]={englishStrings}; // add more languages here.
+const stringsTable_t* currentLanguage=&languages[0]; // default to English.
+
