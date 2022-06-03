d9dd99f9de48aa5dd9f9939037404a9067f1ab8b
vk7js <58905135+vk7js@users.noreply.github.com>
2022-06-03 22:27:12 +1000

    Added a couple of comments and added whitespace between a couple of functions. No code changes.

--- openrtx/src/ui/ui.c
+++ openrtx/src/ui/ui.c
@@ -83,6 +83,7 @@
 static uint16_t functionLatchTimer = 0;
 // 3000 ms.
 #define FUNCTION_LATCH_TIMEOUT 3000
+
 // When a key is pressed while Moni is latched, the latch timer is restarted.
 static void RestartFunctionLatchTimer()
 {
@@ -99,16 +100,20 @@ static void ReleaseFunctionLatchIfNeeded()
 		return;
 	
 	functionLatchTimer=0;
+	// Play beep for function latch release.
 }
 
 static void SetFunctionLatchTimer()
 {
 	functionLatchTimer= getTick() + FUNCTION_LATCH_TIMEOUT;
+	// Play beep for set function latch.
 }
+
 static bool FunctionKeyIsLatched()
 {
 	return (functionLatchTimer > 0) && (getTick() < functionLatchTimer);
 }
+
 /* UI main screen functions, their implementation is in "ui_main.c" */
 extern void _ui_drawMainBackground();
 extern void _ui_drawMainTop();
@@ -1090,7 +1095,6 @@ void ui_updateFSM(event_t event, bool *sync_rtx)
 			if (moniPressed && msg.long_press && !input_getPressedNumber(msg))
 			{
 				SetFunctionLatchTimer(); // 3000 ms.
-				// Need to play beep to alert latch state enabled.
 			}
 			else
 			{
