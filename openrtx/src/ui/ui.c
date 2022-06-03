bec9ec02186b089c783c1bbf543d991e0254af4f
vk7js <58905135+vk7js@users.noreply.github.com>
2022-06-03 22:22:08 +1000

    Added call to ReleaseFunctionLatchIfNeeded to status event branch of fsm so that the function latch will be released when needed without having to wait for a key press.

--- openrtx/src/ui/ui.c
+++ openrtx/src/ui/ui.c
@@ -1835,6 +1835,8 @@ void ui_updateFSM(bool *sync_rtx)
     }
     else if(event.type == EVENT_STATUS)
     {
+		ReleaseFunctionLatchIfNeeded();
+
         if (txOngoing || rtx_rxSquelchOpen())
         {
             _ui_exitStandby(now);
