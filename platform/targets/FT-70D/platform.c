b099814b8ccc993d55021e92184a589c9bf68f6d
Silvano Seva <silseva@fastwebnet.it>
2022-10-11 11:58:59 +0200

    Added (empty) stub files for FT-70D target

---
+++ platform/targets/FT-70D/platform.c
@@ -0,0 +1,99 @@
+/***************************************************************************
+ *   Copyright (C) 2022 by Federico Amedeo Izzo IU2NUO,                    *
+ *                         Niccolò Izzo IU2KIN,                            *
+ *                         Frederik Saraci IU2NRO,                         *
+ *                         Silvano Seva IU2KWO                             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
+ ***************************************************************************/
+
+#include <interfaces/platform.h>
+#include <hwconfig.h>
+#include <stddef.h>
+
+void platform_init()
+{
+
+}
+
+void platform_terminate()
+{
+
+}
+
+uint16_t platform_getVbat()
+{
+   return 0;
+}
+
+uint8_t platform_getMicLevel()
+{
+    return 0;
+}
+
+uint8_t platform_getVolumeLevel()
+{
+    return 0;
+}
+
+int8_t platform_getChSelector()
+{
+    return 0;
+}
+
+bool platform_getPttStatus()
+{
+    return false;
+}
+
+bool platform_pwrButtonStatus()
+{
+    return true;
+}
+
+void platform_ledOn(led_t led)
+{
+    (void) led;
+}
+
+void platform_ledOff(led_t led)
+{
+    (void) led;
+}
+
+void platform_beepStart(uint16_t freq)
+{
+    /* TODO */
+    (void) freq;
+}
+
+void platform_beepStop()
+{
+    /* TODO */
+}
+
+const void *platform_getCalibrationData()
+{
+    return NULL;
+}
+
+const hwInfo_t *platform_getHwInfo()
+{
+    return NULL;
+}
+
+void platform_setBacklightLevel(uint8_t level)
+{
+    (void) level;
+}
