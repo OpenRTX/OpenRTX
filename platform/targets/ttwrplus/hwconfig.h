7c2c61f948e67dbdff72023dc1120f43e693425f
Silvano Seva <silseva@fastwebnet.it>
2023-07-26 12:04:10 +0200

    Added Lilygo T-TWR Plus target, using stub drivers.

---
+++ platform/targets/ttwrplus/hwconfig.h
@@ -0,0 +1,36 @@
+/***************************************************************************
+ *   Copyright (C) 2023 by Federico Amedeo Izzo IU2NUO,                    *
+ *                         Niccolò Izzo IU2KIN                             *
+ *                         Silvano Seva IU2KWO                             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
+ ***************************************************************************/
+
+#ifndef HWCONFIG_H
+#define HWCONFIG_H
+
+#include <zephyr/device.h>
+
+/*
+ * Display properties are encoded in the devicetree
+ */
+#define DISPLAY       DT_CHOSEN(zephyr_display)
+#define SCREEN_WIDTH  DT_PROP(DISPLAY, width)
+#define SCREEN_HEIGHT DT_PROP(DISPLAY, height)
+#define PIX_FMT_BW
+
+/* TODO: battery type */
+#define BAT_NONE
+
+#endif /* HWCONFIG_H */
---
+++ platform/targets/ttwrplus/platform.c
@@ -0,0 +1,93 @@
+/***************************************************************************
+ *   Copyright (C) 2023 by Federico Amedeo Izzo IU2NUO,                    *
+ *                         Niccolò Izzo IU2KIN                             *
+ *                         Silvano Seva IU2KWO                             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
+ ***************************************************************************/
+
+#include <interfaces/platform.h>
+#include <hwconfig.h>
+
+static const hwInfo_t hwInfo =
+{
+    .uhf_maxFreq = 430,
+    .uhf_minFreq = 440,
+    .uhf_band    = 1,
+    .name        = "ttwrplus"
+};
+
+
+void platform_init()
+{
+}
+
+void platform_terminate()
+{
+}
+
+uint16_t platform_getVbat()
+{
+    return 0;
+}
+
+uint8_t platform_getMicLevel()
+{
+    return 0;
+}
+
+uint8_t platform_getVolumeLevel()
+{
+    return 0;
+}
+
+int8_t platform_getChSelector()
+{
+    return 0;
+}
+
+bool platform_getPttStatus()
+{
+    return false;
+}
+
+bool platform_pwrButtonStatus()
+{
+    return true;
+}
+
+void platform_ledOn(led_t led)
+{
+    (void) led;
+}
+
+void platform_ledOff(led_t led)
+{
+    (void) led;
+}
+
+void platform_beepStart(uint16_t freq)
+{
+    (void) freq;
+}
+
+void platform_beepStop()
+{
+}
+
+const hwInfo_t *platform_getHwInfo()
+{
+    return &hwInfo;
+}
+
