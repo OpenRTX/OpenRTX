9e10b687f9a413b8639956d3d24ad5c5d99bfb8d
Marco <49691247+marcoSchr@users.noreply.github.com>
2023-08-24 22:54:41 +0200

    Add symbols for M17 stream data display

--- openrtx/include/fonts/symbols/Symbols5pt7b.h
+++ openrtx/include/fonts/symbols/Symbols5pt7b.h
@@ -1,11 +1,16 @@
 // This is a generated file, please do not edit it! Use generate_symbols.py
 static const uint8_t Symbols5pt7bBitmaps[] PROGMEM = {
 0x00, //space
+0x00, 0x00, 0x42, 0x24, 0x24, 0x42, 0x00, 0x00, //access-point.svg
 0x00, 0x7e, 0x5a, 0x66, 0x66, 0x42, 0x7e, 0x00, //alpha-m-box-outline.svg
+0x00, 0x00, 0x04, 0x08, 0x10, 0x20, 0x00, 0x00, //call-made.svg
+0x00, 0x00, 0x04, 0x08, 0x10, 0x20, 0x00, 0x00, //call-received.svg
 0x00, 0x18, 0x7e, 0x66, 0x66, 0x7e, 0x18, 0x00, //cog.svg
 0x00, 0x3c, 0x42, 0x5a, 0x5a, 0x42, 0x3c, 0x00, //crosshairs-gps.svg
 0x00, 0x3c, 0x42, 0x42, 0x42, 0x42, 0x3c, 0x00, //crosshairs.svg
 0x18, 0x24, 0x24, 0x7e, 0x7e, 0x7e, 0x7e, 0x00, //lock.svg
+0x00, 0x3c, 0x3c, 0x3c, 0x3c, 0x3c, 0x18, 0x18, //network.svg
+0x00, 0x00, 0x18, 0x18, 0x18, 0x18, 0x24, 0x00, //transmission-tower.svg
 0x00, 0x3c, 0x81, 0x42, 0x3c, 0x18, 0x18, 0x00, //wifi-strength-1.svg
 0x00, 0x3c, 0x81, 0x5a, 0x3c, 0x18, 0x18, 0x00, //wifi-strength-2.svg
 0x00, 0x3c, 0x81, 0x7e, 0x3c, 0x18, 0x18, 0x00, //wifi-strength-3.svg
@@ -15,18 +20,23 @@ static const uint8_t Symbols5pt7bBitmaps[] PROGMEM = {
 static const GFXglyph Symbols5pt7bGlyphs[] PROGMEM = {
   //Index,  W, H,xAdv,dX, dY
 { 0, 1, 1, 5, 0, -7},   // " " space
-{ 1, 8, 8, 5, 0, -7},   // "!" alpha-m-box-outline.svg
-{ 9, 8, 8, 5, 0, -7},   // """ cog.svg
-{ 17, 8, 8, 5, 0, -7},   // "#" crosshairs-gps.svg
-{ 25, 8, 8, 5, 0, -7},   // "$" crosshairs.svg
-{ 33, 8, 8, 5, 0, -7},   // "%" lock.svg
-{ 41, 8, 8, 5, 0, -7},   // "&" wifi-strength-1.svg
-{ 49, 8, 8, 5, 0, -7},   // "'" wifi-strength-2.svg
-{ 57, 8, 8, 5, 0, -7},   // "(" wifi-strength-3.svg
-{ 65, 8, 8, 5, 0, -7},   // ")" wifi-strength-4.svg
+{ 1, 8, 8, 5, 0, -7},   // "!" access-point.svg
+{ 9, 8, 8, 5, 0, -7},   // """ alpha-m-box-outline.svg
+{ 17, 8, 8, 5, 0, -7},   // "#" call-made.svg
+{ 25, 8, 8, 5, 0, -7},   // "$" call-received.svg
+{ 33, 8, 8, 5, 0, -7},   // "%" cog.svg
+{ 41, 8, 8, 5, 0, -7},   // "&" crosshairs-gps.svg
+{ 49, 8, 8, 5, 0, -7},   // "'" crosshairs.svg
+{ 57, 8, 8, 5, 0, -7},   // "(" lock.svg
+{ 65, 8, 8, 5, 0, -7},   // ")" network.svg
+{ 73, 8, 8, 5, 0, -7},   // "*" transmission-tower.svg
+{ 81, 8, 8, 5, 0, -7},   // "+" wifi-strength-1.svg
+{ 89, 8, 8, 5, 0, -7},   // "," wifi-strength-2.svg
+{ 97, 8, 8, 5, 0, -7},   // "-" wifi-strength-3.svg
+{ 105, 8, 8, 5, 0, -7},   // "." wifi-strength-4.svg
 };
 static const GFXfont Symbols5pt7b PROGMEM = {
 (uint8_t  *)Symbols5pt7bBitmaps,
 (GFXglyph *)Symbols5pt7bGlyphs,
 //ASCII start, ASCII stop,y Advance 
-  32, 41, 19 };
+  32, 46, 19 };
