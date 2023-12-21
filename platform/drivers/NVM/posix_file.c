0be6c74b63a5f7e700a48ea482d9970b5e844c9c
Niccolò Izzo <izzo.niccolo@gmail.com>
2023-12-21 09:50:41 +0100

    Fix off-by-one errors
    Fixed identical off-by-one errors in blockdevice read function and in
    linux posix file driver.

--- platform/drivers/NVM/posix_file.c
+++ platform/drivers/NVM/posix_file.c
@@ -74,7 +74,7 @@ static int nvm_api_read(const struct nvmDevice *dev, uint32_t offset,
     if(pDev->fd < 0)
         return -EBADF;
 
-    if((offset + len) >= pDev->size)
+    if((offset + len) > pDev->size)
         return -EINVAL;
 
     lseek(pDev->fd, offset, SEEK_SET);
