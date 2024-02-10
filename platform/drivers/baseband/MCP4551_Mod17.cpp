43694bd2ce37c5156cc9670bde48ee4d08c2ea87
Morgan Diepart <morgandiepart@gmail.com>
2024-02-10 18:09:59 +0100

    Use hardware I2C for MCP4551 instead of software I2C

--- platform/drivers/baseband/MCP4551_Mod17.cpp
+++ platform/drivers/baseband/MCP4551_Mod17.cpp
@@ -19,28 +19,12 @@
  *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
  ***************************************************************************/
 
+#include "MCP4551.h"
+#include "I2C1.h"
 #include <peripherals/gpio.h>
 #include <interfaces/delays.h>
 #include <hwconfig.h>
-#include "MCP4551.h"
 
-/*
- * Implementation of MCP4551 I2C interface.
- *
- * Hardware I2C is not yet implemented. Bit-bang, baby!
- */
-
-void _i2c_start();
-void _i2c_stop();
-void _i2c_write(uint8_t val);
-uint8_t _i2c_read(bool ack);
-
-void i2c_init()
-{
-    gpio_setMode(I2C_SDA, INPUT);
-    gpio_setMode(I2C_SCL, OUTPUT);
-    gpio_clearPin(I2C_SCL);
-}
 
 void mcp4551_init(uint8_t addr)
 {
@@ -49,155 +33,10 @@ void mcp4551_init(uint8_t addr)
 
 void mcp4551_setWiper(uint8_t devAddr, uint16_t value)
 {
-    _i2c_start();
-    _i2c_write(devAddr << 1);
-    uint8_t temp = ((value >> 8 & 0x01) | MCP4551_CMD_WRITE);
-    _i2c_write(temp);
-    temp = (value & 0xFF);
-    _i2c_write(temp);
-    _i2c_stop();
-}
+    uint8_t data[2] = {(uint8_t)(value >> 8 & 0x01)|MCP4551_CMD_WRITE,
+                        (uint8_t)value};
 
-/*uint16_t i2c_readReg16(uint8_t devAddr, uint8_t reg)
-{
-    _i2c_start();
-    _i2c_write(devAddr << 1);
-    _i2c_write(reg);
-    _i2c_start();
-    _i2c_write(devAddr | 0x01);
-    uint8_t valHi = _i2c_read(true);
-    uint8_t valLo = _i2c_read(false);
-    _i2c_stop();
-
-    return (valHi << 8) | valLo;
-} */
-
-/*
- * Software I2C routine
- */
-
-void _i2c_start()
-{
-    gpio_setMode(I2C_SDA, OUTPUT);
-
-    /*
-     * Lines commented to keep SCL high when idle
-     *
-    gpio_clearPin(I2C_SCL);
-    delayUs(2);
-    */
-
-    gpio_setPin(I2C_SDA);
-    delayUs(5);
-
-    gpio_setPin(I2C_SCL);
-    delayUs(5);
-
-    gpio_clearPin(I2C_SDA);
-    delayUs(5);
-
-    gpio_clearPin(I2C_SCL);
-    delayUs(6);
-}
-
-void _i2c_stop()
-{
-    gpio_setMode(I2C_SDA, OUTPUT);
-
-    gpio_clearPin(I2C_SCL);
-    delayUs(5);
-
-    gpio_clearPin(I2C_SDA);
-    delayUs(5);
-
-    gpio_setPin(I2C_SCL);
-    delayUs(5);
-
-    gpio_setPin(I2C_SDA);
-    delayUs(5);
-
-    /*
-     * Lines commented to keep SCL high when idle
-     *
-    gpio_clearPin(I2C_SCL);
-    delayUs(5);
-    */
-}
-
-void _i2c_write(uint8_t val)
-{
-    gpio_setMode(I2C_SDA, OUTPUT);
-
-    for(uint8_t i = 0; i < 8; i++)
-    {
-        gpio_clearPin(I2C_SCL);
-        delayUs(1);
-
-        if(val & 0x80)
-        {
-            gpio_setPin(I2C_SDA);
-        }
-        else
-        {
-            gpio_clearPin(I2C_SDA);
-        }
-
-        val <<= 1;
-        delayUs(1);
-        gpio_setPin(I2C_SCL);
-        delayUs(5);
-    }
-
-    /* Ensure SCL is low before releasing SDA */
-    gpio_clearPin(I2C_SCL);
-
-    /* Clock cycle for slave ACK/NACK */
-    gpio_setMode(I2C_SDA, INPUT_PULL_UP);
-    delayUs(5);
-    gpio_setPin(I2C_SCL);
-    delayUs(5);
-    gpio_clearPin(I2C_SCL);
-    delayUs(1);
-
-    /* Asserting SDA pin allows to fastly bring the line to idle state */
-    gpio_setPin(I2C_SDA);
-    gpio_setMode(I2C_SDA, OUTPUT);
-    delayUs(6);
-}
-
-uint8_t _i2c_read(bool ack)
-{
-    gpio_setMode(I2C_SDA, INPUT_PULL_UP);
-    gpio_clearPin(I2C_SCL);
-
-    uint8_t value = 0;
-    for(uint8_t i = 0; i < 8; i++)
-    {
-        delayUs(5);
-        gpio_setPin(I2C_SCL);
-        delayUs(5);
-
-        value <<= 1;
-        value |= gpio_readPin(I2C_SDA);
-
-        gpio_clearPin(I2C_SCL);
-    }
-
-    /*
-     * Set ACK/NACK state BEFORE putting SDA gpio to output mode.
-     * This avoids spurious spikes which can be interpreted as NACKs
-     */
-    gpio_clearPin(I2C_SDA);
-    gpio_setMode(I2C_SDA, OUTPUT);
-    delayUs(5);
-    if(!ack) gpio_setPin(I2C_SDA);
-
-    /* Clock cycle for ACK/NACK */
-    delayUs(5);
-    gpio_setPin(I2C_SCL);
-    delayUs(5);
-    gpio_clearPin(I2C_SCL);
-    delayUs(5);
-
-    return value;
+    i2c1_lockDeviceBlocking();
+    i2c1_write_bytes(devAddr, data, 2, true);
+    i2c1_releaseDevice();
 }
