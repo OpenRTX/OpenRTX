f1accc88d2e05cd7cf4f96d765295c3ff45336c2
Morgan Diepart <morgandiepart@gmail.com>
2024-02-14 18:55:29 +0100

    Restored openrtx settings of miosix

--- lib/miosix-kernel/miosix/config/miosix_settings.h
+++ lib/miosix-kernel/miosix/config/miosix_settings.h
@@ -32,7 +32,7 @@
 // The PARSING_FROM_IDE is because Netbeans gets confused by this, it is never
 // defined when compiling the code.
 #ifndef PARSING_FROM_IDE
-#error This error is a reminder that you have not edited miosix_settings.h yet.
+// #error This error is a reminder that you have not edited miosix_settings.h yet.
 #endif //PARSING_FROM_IDE
 
 /**
@@ -89,12 +89,12 @@ namespace miosix {
 /// \def WITH_FILESYSTEM
 /// Allows to enable/disable filesystem support to save code size
 /// By default it is defined (filesystem support is enabled)
-#define WITH_FILESYSTEM
+// #define WITH_FILESYSTEM
 
 /// \def WITH_DEVFS
 /// Allows to enable/disable DevFs support to save code size
 /// By default it is defined (DevFs is enabled)
-#define WITH_DEVFS
+// #define WITH_DEVFS
     
 /// \def SYNC_AFTER_WRITE
 /// Increases filesystem write robustness. After each write operation the
@@ -102,7 +102,7 @@ namespace miosix {
 /// (unless power failure happens exactly between the write and the sync)
 /// Unfortunately write latency and throughput becomes twice as worse
 /// By default it is defined (slow but safe)
-#define SYNC_AFTER_WRITE
+// #define SYNC_AFTER_WRITE
 
 /// Maximum number of open files. Trying to open more will fail.
 /// Cannot be lower than 3, as the first three are stdin, stdout, stderr
@@ -134,12 +134,12 @@ const unsigned char MAX_OPEN_FILES=8;
 /// \def WITH_BOOTLOG
 /// Uncomment to print bootlogs on stdout.
 /// By default it is defined (bootlogs are printed)
-#define WITH_BOOTLOG
+// #define WITH_BOOTLOG
 
 /// \def WITH_ERRLOG
 /// Uncomment for debug information on stdout.
 /// By default it is defined (error information is printed)
-#define WITH_ERRLOG
+// #define WITH_ERRLOG
 
 
 
