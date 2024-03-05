71b35985cea81fb7e61d834abd09abf67f83ac53
jpucheu <jpucheu@gmail.com>
2024-03-05 09:29:24 -0300

    UI: added string table for Spanish language

---
+++ openrtx/include/ui/SpanishStrings.h
@@ -0,0 +1,100 @@
+/****************************************************************************
+ *   Copyright (C) 2024 by Federico Amedeo Izzo IU2NUO,                    *
+ *                         Niccolò Izzo IU2KIN                             *
+ *                         Frederik Saraci IU2NRO                          *
+ *                         Silvano Seva IU2KWO                             *
+ *                         Joseph Stephen VK7JS                            *
+ *                         Juan I. Pucheu LW7EMN                           *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
+ ***************************************************************************/
+#ifndef SPANISHSTRINGS_H
+#define SPANISHSTRINGS_H
+
+#include "ui/ui_strings.h"
+
+const stringsTable_t spanishStrings =
+{
+    .languageName      = "Español",
+    .off               = "Apagado",
+    .on                = "Encendido",
+    .banks             = "Banks",
+    .channels          = "Canales",
+    .contacts          = "Contactos",
+    .gps               = "GPS",
+    .settings          = "Ajustes",
+    .backupAndRestore  = "Backup & Restaurar",
+    .info              = "Info",
+    .about             = "Acerca de",
+    .display           = "Display",
+    .timeAndDate       = "Fecha & Hora",
+    .fm                = "FM",
+    .m17               = "M17",
+    .dmr               = "DMR",
+    .defaultSettings   = "Ajustes de fábrica",
+    .brightness        = "Brillo",
+    .contrast          = "Contraste",
+    .timer             = "Cronómetro",
+    .gpsEnabled        = "GPS habilitado",
+    .gpsSetTime        = "GPS Ajustar hora",
+    .UTCTimeZone       = "Zona Horaria UTC",
+    .voice             = "Voz",
+    .level             = "Nivel",
+    .phonetic          = "Fonético",
+    .beep              = "Bip",
+    .backup            = "Backup",
+    .restore           = "Restarurar",
+    .batteryVoltage    = "Voltage Bat.",
+    .batteryCharge     = "Carga Bat.",
+    .RSSI              = "RSSI",
+    .model             = "Modelo",
+    .band              = "Banda",
+    .VHF               = "VHF",
+    .UHF               = "UHF",
+    .LCDType           = "Tipo de LCD",
+    .Niccolo           = "Niccolo' IU2KIN",
+    .Silvano           = "Silvano IU2KWO",
+    .Federico          = "Federico IU2NUO",
+    .Fred              = "Fred IU2NRO",
+    .Joseph            = "Joseph VK7JS",
+    .allChannels       = "Todos los canales",
+    .menu              = "Menu",
+    .gpsOff            = "GPS Apagado",
+    .noFix             = "Sin posición",
+    .fixLost           = "Posición perdida",
+    .error             = "ERROR",
+    .flashBackup       = "Guardar Backup",
+    .connectToRTXTool  = "Conectar con RTXTool",
+    .toBackupFlashAnd  = "guardar flash y",
+    .pressPTTToStart   = "Apretar PTT para iniciar",
+    .flashRestore      = "Flash restore",
+    .toRestoreFlashAnd = "to restore flash and",
+    .openRTX           = "OpenRTX",
+    .gpsSettings       = "Ajustes de GPS",
+    .m17settings       = "Ajustes de M17",
+    .callsign          = "Licencia:",
+    .resetToDefaults   = "Reseatear a fábrica",
+    .toReset           = "Para resetear:",
+    .pressEnterTwice   = "Apretar Enter 2 veces",
+    .macroMenu         = "Menú macro",
+    .forEmergencyUse   = "Sólo Emergencias",
+    .pressAnyButton    = "Apretar cualquier botón.",
+    .accessibility     = "Accesibilidad",
+    .usedHeap          = "Memoria heap usada",
+    .broadcast         = "TODOS",
+    .radioSettings     = "Ajustes de Radio",
+    .frequencyOffset   = "Offset de frecuencia",
+    .macroLatching     = "Macro Latching"
+};
+#endif  // SPANISHSTRINGS_H
