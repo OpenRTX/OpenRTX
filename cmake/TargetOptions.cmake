63f605013dec9a8308b2d308d5943599ae8fc602
Alain Carlucci <carpikemail@gmail.com>
2024-04-10 21:11:45 +0200

    update

--- cmake/TargetOptions.cmake
+++ cmake/TargetOptions.cmake
@@ -1,4 +1,8 @@
 if(OPENRTX_TARGET STREQUAL "linux")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -ffunction-sections -fdata-sections")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -ffunction-sections -fdata-sections")
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,--gc-sections")
+
     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -g -fsanitize=address,undefined")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g -fsanitize=address,undefined")
 
@@ -6,7 +10,39 @@ if(OPENRTX_TARGET STREQUAL "linux")
     find_package(Codec2 REQUIRED)
 
     add_compile_definitions(-DPLATFORM_LINUX)
-    add_compile_definitions(-DSCREEN_WIDTH=160 -DSCREEN_HEIGHT=128 -DPIX_FMT_RGB565)
+    add_compile_definitions(-Dsniprintf=snprintf -Dvsniprintf=vsnprintf)
+
+    if(OPENRTX_LINUX_FLAVOR STREQUAL "default")
+        add_compile_definitions(
+            -DCONFIG_SCREEN_WIDTH=160
+            -DCONFIG_SCREEN_HEIGHT=128
+            -DCONFIG_PIX_FMT_RGB565
+            -DCONFIG_GPS
+            -DCONFIG_RTC
+        )
+    elseif(OPENRTX_LINUX_FLAVOR STREQUAL "small")
+        add_compile_definitions(
+            -DCONFIG_SCREEN_WIDTH=128
+            -DCONFIG_SCREEN_HEIGHT=64
+            -DCONFIG_PIX_FMT_BW
+            -DCONFIG_GPS
+            -DCONFIG_RTC
+        )
+    elseif(OPENRTX_LINUX_FLAVOR STREQUAL "module17")
+        set(OPENRTX_UI module17)
+        add_compile_definitions(
+            -DCONFIG_SCREEN_WIDTH=128
+            -DCONFIG_SCREEN_HEIGHT=64
+            -DCONFIG_PIX_FMT_BW
+        )
+    else()
+        message(FATAL_ERROR 
+            "Invalid variable OPENRTX_LINUX_FLAVOR. "
+            "Please choose one between: default, small, module17.")
+    endif()
+
+    message("Using linux flavor: ${OPENRTX_LINUX_FLAVOR}")
+
 elseif(OPENRTX_TARGET STREQUAL "md3x0")
     add_compile_definitions(-DPLATFORM_MD3x0 -Dtimegm=mktime)
     add_compile_definitions(STM32F405xx HSE_VALUE=8000000)
@@ -35,5 +71,6 @@ elseif(OPENRTX_TARGET STREQUAL "dm1801")
 
     set(TARGET_WRAP DM-1801)
     set(TARGET_LOAD_ADDR 0x0800C000)
-elseif(OPENRTX_TARGET STREQUAL "mod17")
+elseif(OPENRTX_TARGET STREQUAL "module17")
+    add_compile_definitions(-DPLATFORM_MOD17)
 endif()
