a4db8d22421ef1387ade67aef72a32b352d2f473
Silvano Seva <silseva@fastwebnet.it>
2024-06-02 15:08:54 +0200

    NVM: deeply restructured W25Qx driver
    Refactored the W25Qx driver in order to make it an SPI device conformant
    to the nvmDevice driver interface. Removed the read/write/erase functions.

--- platform/drivers/NVM/W25Qx.h
+++ platform/drivers/NVM/W25Qx.h
@@ -24,6 +24,8 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <sys/types.h>
+#include <peripherals/spi.h>
+#include <peripherals/gpio.h>
 #include <interfaces/nvmem.h>
 
 /**
@@ -32,9 +34,18 @@
  */
 
 /**
+ * Configuration data structure for W25Qx memory device.
+ */
+struct W25QxCfg
+{
+    const struct spiDevice *spi;
+    const struct gpioPin    cs;
+};
+
+/**
  * Driver data structure for W25Qx security registers.
  */
-struct w25qSecRegDevice
+struct W25QxSecRegDevice
 {
     const void           *priv;        ///< Device driver private data
     const struct nvmOps  *ops;         ///< Device operations
@@ -55,12 +66,13 @@ extern const struct nvmInfo W25Qx_info;
  * @param name: device name.
  * @param sz: memory size, in bytes.
  */
-#define W25Qx_DEVICE_DEFINE(name, sz) \
-struct nvmDevice name =               \
-{                                     \
-    .ops  = &W25Qx_ops,               \
-    .info = &W25Qx_info,              \
-    .size = sz                        \
+#define W25Qx_DEVICE_DEFINE(name, config, sz) \
+const struct nvmDevice name =                 \
+{                                             \
+    .priv = &config,                          \
+    .ops  = &W25Qx_ops,                       \
+    .info = &W25Qx_info,                      \
+    .size = sz,                               \
 };
 
 /**
@@ -76,24 +88,25 @@ extern const struct nvmInfo W25Qx_secReg_info;
  * @param base: security register base address.
  * @param sz: memory size, in bytes.
  */
-#define W25Qx_SECREG_DEFINE(name, base, sz) \
-struct w25qSecRegDevice name =              \
-{                                           \
-    .ops      = &W25Qx_secReg_ops,          \
-    .info     = &W25Qx_secReg_info,         \
-    .size     = sz,                         \
-    .baseAddr = base                        \
+#define W25Qx_SECREG_DEFINE(name, config, base, sz) \
+const struct W25QxSecRegDevice name =               \
+{                                                   \
+    .priv     = &config,                            \
+    .ops      = &W25Qx_secReg_ops,                  \
+    .info     = &W25Qx_secReg_info,                 \
+    .size     = sz,                                 \
+    .baseAddr = base                                \
 };
 
 /**
  * Initialise driver for external flash.
  */
-void W25Qx_init();
+void W25Qx_init(const struct nvmDevice *dev);
 
 /**
  * Terminate driver for external flash.
  */
-void W25Qx_terminate();
+void W25Qx_terminate(const struct nvmDevice *dev);
 
 /**
  * Release flash chip from power down mode, this function should be called at
@@ -102,76 +115,11 @@ void W25Qx_terminate();
  * Application code must wait at least 3us before issuing any other command
  * after this one.
  */
-void W25Qx_wakeup();
+void W25Qx_wakeup(const struct nvmDevice *dev);
 
 /**
  * Put flash chip in low power mode.
  */
-void W25Qx_sleep();
-
-/**
- * Read data from one of the flash security registers, located at addresses
- * 0x1000, 0x2000 and 0x3000 and 256-byte wide.
- * NOTE: If a read operation goes beyond the 256 byte boundary, length will be
- * truncated to the one reaching the end of the register.
- *
- * @param addr: start address for read operation, must be the full register address.
- * @param buf: pointer to a buffer where data is written to.
- * @param len: number of bytes to read.
- * @return: -1 if address is not whithin security registers address range, the
- * number of bytes effectively read otherwise.
- */
-ssize_t W25Qx_readSecurityRegister(uint32_t addr, void *buf, size_t len);
-
-/**
- * Read data from flash memory.
- *
- * @param addr: start address for read operation.
- * @param buf: pointer to a buffer where data is written to.
- * @param len: number of bytes to read.
- * @return zero on success, negative errno code on fail.
- */
-int W25Qx_readData(uint32_t addr, void *buf, size_t len);
-
-/**
- * Erase a flash memory area.
- * The start address and erase size must be aligned to page size. The function
- * blocks until the erase process terminates.
- *
- * @param addr: start address.
- * @param size: size of the area to be erased.
- * @return zero on success, negative errno code on fail.
- */
-int W25Qx_erase(uint32_t addr, size_t size);
-
-/**
- * Full chip erase.
- * Function returns when erase process terminated.
- *
- * @return true on success, false on failure.
- */
-bool W25Qx_eraseChip();
-
-/**
- * Write data to a 256-byte flash memory page.
- * NOTE: if data size goes beyond the 256 byte boundary, length will be truncated
- * to the one reaching the end of the page.
- *
- * @param addr: start address for write operation.
- * @param buf: pointer to data buffer.
- * @param len: number of bytes to written.
- * @return: -1 on error, the number of bytes effectively written otherwise.
- */
-ssize_t W25Qx_writePage(uint32_t addr, const void *buf, size_t len);
-
-/**
- * Write data to flash memory.
- *
- * @param addr: start address for read operation.
- * @param buf: pointer to a buffer where data is written to.
- * @param len: number of bytes to read.
- * @return zero on success, negative errno code on fail.
- */
-int W25Qx_writeData(uint32_t addr, const void *buf, size_t len);
+void W25Qx_sleep(const struct nvmDevice *dev);
 
 #endif /* W25Qx_H */
