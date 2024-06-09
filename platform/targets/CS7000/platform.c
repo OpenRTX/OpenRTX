d3fa6e289b9e14032e0603da7fa51865c5e891a9
Silvano Seva <silseva@fastwebnet.it>
2024-06-09 14:33:05 +0200

    CS7000: audio driver

--- platform/targets/CS7000/platform.c
+++ platform/targets/CS7000/platform.c
@@ -18,6 +18,7 @@
 #include <interfaces/platform.h>
 #include <peripherals/gpio.h>
 #include <interfaces/nvmem.h>
+#include <interfaces/audio.h>
 #include <gpio_shiftReg.h>
 #include <spi_bitbang.h>
 #include <adc_stm32.h>
@@ -50,6 +51,7 @@ void platform_init()
     gpioShiftReg_init(&extGpio);
     adcStm32_init(&adc1);
     nvm_init();
+    audio_init();
 
     #ifndef RUNNING_TESTSUITE
     gpioDev_set(MAIN_PWR_SW);
