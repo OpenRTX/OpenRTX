632303ef78004552928bc7b45690809bec4cb1cf
Andrej <andrej@case.edu>
2024-07-09 20:30:46 +0200

    a36plus: Swap columns and rows; add macro key

--- platform/drivers/keyboard/keyboard_a36plus.c
+++ platform/drivers/keyboard/keyboard_a36plus.c
@@ -28,77 +28,77 @@
 void kbd_init()
 {
     /* Set the two column lines as outputs */
-    gpio_setMode(KBD_COL0, OUTPUT);
-    gpio_setMode(KBD_COL1, OUTPUT);
-    gpio_setMode(KBD_COL2, OUTPUT);
-    gpio_setMode(KBD_COL3, OUTPUT);
+    gpio_setMode(KBD_ROW0, OUTPUT);
+    gpio_setMode(KBD_ROW1, OUTPUT);
+    gpio_setMode(KBD_ROW2, OUTPUT);
+    gpio_setMode(KBD_ROW3, OUTPUT);
 
-    gpio_setMode(KBD_ROW0, INPUT_PULL_UP);
-    gpio_setMode(KBD_ROW1, INPUT_PULL_UP);
-    gpio_setMode(KBD_ROW2, INPUT_PULL_UP);
-    gpio_setMode(KBD_ROW3, INPUT_PULL_UP);
-    gpio_setMode(SKB_1,    INPUT_PULL_UP);
+    gpio_setMode(KBD_COL0, INPUT_PULL_UP);
+    gpio_setMode(KBD_COL1, INPUT_PULL_UP);
+    gpio_setMode(KBD_COL2, INPUT_PULL_UP);
+    gpio_setMode(KBD_COL3, INPUT_PULL_UP);
+    gpio_setMode(SKB_1,    INPUT);
 
-    gpio_setPin(KBD_COL0);
-    gpio_setPin(KBD_COL1);
-    gpio_setPin(KBD_COL2);
-    gpio_setPin(KBD_COL3);
+    gpio_setPin(KBD_ROW0);
+    gpio_setPin(KBD_ROW1);
+    gpio_setPin(KBD_ROW2);
+    gpio_setPin(KBD_ROW3);
 }
 
 void kbd_terminate()
 {
     /* Back to default state */
-    gpio_clearPin(KBD_COL0);
-    gpio_clearPin(KBD_COL1);
-    gpio_clearPin(KBD_COL2);
-    gpio_clearPin(KBD_COL3);
-    gpio_setMode(KBD_COL0, INPUT);
-    gpio_setMode(KBD_COL1, INPUT);
-    gpio_setMode(KBD_COL2, INPUT);
-    gpio_setMode(KBD_COL3, INPUT);
+    gpio_clearPin(KBD_ROW0);
+    gpio_clearPin(KBD_ROW1);
+    gpio_clearPin(KBD_ROW2);
+    gpio_clearPin(KBD_ROW3);
+    gpio_setMode(KBD_ROW0, INPUT);
+    gpio_setMode(KBD_ROW1, INPUT);
+    gpio_setMode(KBD_ROW2, INPUT);
+    gpio_setMode(KBD_ROW3, INPUT);
 }
 
 keyboard_t kbd_getKeys()
 {
     keyboard_t keys = 0;
-
+    
     if(!gpio_readPin(SKB_1)) keys |= KEY_MONI;
-
-    gpio_setPin(KBD_COL3);
-    gpio_clearPin(KBD_COL0);
-
-    delayUs(10);
-    if(!gpio_readPin(KBD_ROW0)) keys |= KEY_ENTER;
-    if(!gpio_readPin(KBD_ROW1)) keys |= KEY_1;
-    if(!gpio_readPin(KBD_ROW2)) keys |= KEY_4;
-    if(!gpio_readPin(KBD_ROW3)) keys |= KEY_7;
-
-    gpio_clearPin(KBD_COL1);
-    gpio_setPin(KBD_COL0);
+    
+    gpio_setPin(KBD_ROW3);
+    gpio_clearPin(KBD_ROW0);
 
     delayUs(10);
-    if(!gpio_readPin(KBD_ROW0)) keys |= KEY_UP;
-    if(!gpio_readPin(KBD_ROW1)) keys |= KEY_2;
-    if(!gpio_readPin(KBD_ROW2)) keys |= KEY_5;
-    if(!gpio_readPin(KBD_ROW3)) keys |= KEY_8;
+    if(!gpio_readPin(KBD_COL0)) keys |= KEY_ENTER;
+    if(!gpio_readPin(KBD_COL1)) keys |= KEY_UP;
+    if(!gpio_readPin(KBD_COL2)) keys |= KEY_DOWN;
+    if(!gpio_readPin(KBD_COL3)) keys |= KEY_ESC;
 
-    gpio_clearPin(KBD_COL2);
-    gpio_setPin(KBD_COL1);
-
-    delayUs(10);
-    if(!gpio_readPin(KBD_ROW0)) keys |= KEY_DOWN;
-    if(!gpio_readPin(KBD_ROW1)) keys |= KEY_3;
-    if(!gpio_readPin(KBD_ROW2)) keys |= KEY_6;
-    if(!gpio_readPin(KBD_ROW3)) keys |= KEY_9;
-
-    gpio_clearPin(KBD_COL3);
-    gpio_setPin(KBD_COL2);
+    gpio_clearPin(KBD_ROW1);
+    gpio_setPin(KBD_ROW0);
 
     delayUs(10);
-    if(!gpio_readPin(KBD_ROW0)) keys |= KEY_ESC;
-    if(!gpio_readPin(KBD_ROW1)) keys |= KEY_STAR;
-    if(!gpio_readPin(KBD_ROW2)) keys |= KEY_0;
-    if(!gpio_readPin(KBD_ROW3)) keys |= KEY_HASH;
+    if(!gpio_readPin(KBD_COL0)) keys |= KEY_1;
+    if(!gpio_readPin(KBD_COL1)) keys |= KEY_2;
+    if(!gpio_readPin(KBD_COL2)) keys |= KEY_3;
+    if(!gpio_readPin(KBD_COL3)) keys |= KEY_STAR;
+
+    gpio_clearPin(KBD_ROW2);
+    gpio_setPin(KBD_ROW1);
+
+    delayUs(10);
+    if(!gpio_readPin(KBD_COL0)) keys |= KEY_4;
+    if(!gpio_readPin(KBD_COL1)) keys |= KEY_5;
+    if(!gpio_readPin(KBD_COL2)) keys |= KEY_6;
+    if(!gpio_readPin(KBD_COL3)) keys |= KEY_0;
+
+    gpio_clearPin(KBD_ROW3);
+    gpio_setPin(KBD_ROW2);
+
+    delayUs(10);
+    if(!gpio_readPin(KBD_COL0)) keys |= KEY_7;
+    if(!gpio_readPin(KBD_COL1)) keys |= KEY_8;
+    if(!gpio_readPin(KBD_COL2)) keys |= KEY_9;
+    if(!gpio_readPin(KBD_COL3)) keys |= KEY_HASH;
 
     return keys;
 }
