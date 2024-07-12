b0328f59415eab0654a2e8d526b94ff1b649f684
Andrej <andrej@case.edu>
2024-07-12 20:12:38 +0200

    Hacks to make the display rendering more bearable for testing

--- openrtx/src/ui/default/ui.c
+++ openrtx/src/ui/default/ui.c
@@ -2597,7 +2597,7 @@ bool ui_updateGUI()
     // If MACRO menu is active draw it
     if(macro_menu)
     {
-        _ui_drawDarkOverlay();
+        //_ui_drawDarkOverlay();
         _ui_drawMacroMenu(&ui_state);
     }
 
