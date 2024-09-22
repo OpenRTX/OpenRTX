ab91c55986b1e570b5123e2dfd2704957ad8838b
Silvano Seva <silseva@fastwebnet.it>
2024-09-22 14:57:29 +0200

    STM32: refactored SPI driver

--- platform/drivers/SPI/spi_stm32.h
+++ platform/drivers/SPI/spi_stm32.h
@@ -23,8 +23,12 @@
 
 #include <peripherals/spi.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /**
- *  Instantiate an STM32 I2C master device.
+ *  Instantiate an STM32 SPI master device.
  *
  * @param name: device name.
  * @param peripheral: underlying MCU peripheral.
@@ -32,8 +36,7 @@
  */
 #define SPI_STM32_DEVICE_DEFINE(name, peripheral, mutx)                      \
 extern int spiStm32_transfer(const struct spiDevice *dev, const void *txBuf, \
-                             const size_t txSize, void *rxBuf,               \
-                             const size_t rxSize);                           \
+                             void *rxBuf, const size_t size);                \
 const struct spiDevice name =                                                \
 {                                                                            \
     .transfer = &spiStm32_transfer,                                          \
@@ -51,13 +54,18 @@ const struct spiDevice name =                                                \
  * @param flags: SPI configuration flags.
  * @return zero on success, a negative error code otherwise.
  */
-int spi_init(const struct spiDevice *dev, const uint32_t speed, const uint8_t flags);
+int spiStm32_init(const struct spiDevice *dev, const uint32_t speed, const uint8_t flags);
 
 /**
  * Shut down an SPI peripheral and driver.
  *
  * @param dev: SPI device descriptor.
  */
-void spi_terminate(const struct spiDevice *dev);
+void spiStm32_terminate(const struct spiDevice *dev);
+
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* SPI_STM32_H */
