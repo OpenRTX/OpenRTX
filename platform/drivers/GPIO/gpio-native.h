4b6e749fc4a8adf725f80c5448f9e4d378b5aa79
Silvano Seva <silseva@fastwebnet.it>
2024-10-12 15:18:00 +0200

    Reorganized GPIO drivers

---
+++ platform/drivers/GPIO/gpio-native.h
@@ -0,0 +1,28 @@
+/***************************************************************************
+ *   Copyright (C) 2024 by Silvano Seva IU2KWO                             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
+ ***************************************************************************/
+
+#ifndef GPIO_NATIVE_H
+#define GPIO_NATIVE_H
+
+#if defined(PLATFORM_MD3x0) || defined(PLATFORM_MDUV3x0) \
+ || defined(PLATFORM_MD9600) || defined(PLATFORM_MOD17)
+#include <gpio_stm32.h>
+#elif defined(PLATFORM_GD77) || defined(PLATFORM_DM1801)
+#include <gpio_mk22.h>
+#endif
+
+#endif /* GPIO_NATIVE_H */
--- platform/drivers/GPIO/gpio_stm32.c
+++ platform/drivers/GPIO/gpio_stm32.c
@@ -17,7 +17,7 @@
 
 #include <errno.h>
 #include "stm32f4xx.h"
-#include "gpio-native.h"
+#include "gpio_stm32.h"
 
 static inline void setGpioAf(GPIO_TypeDef *port, uint8_t pin, const uint8_t af)
 {
