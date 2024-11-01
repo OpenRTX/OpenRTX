e40f31bf7f2a21c73703f1d4b0cdfdd12f39cc13
Andrew Amos <agamos@me.com>
2024-11-01 16:20:51 +0000

    commented  sample data

--- openrtx/src/ui/default/ui_main.c
+++ openrtx/src/ui/default/ui_main.c
@@ -46,14 +46,14 @@ void _ui_drawMainTop(ui_state_t * ui_state)
               color_white, "%02d:%02d:%02d", local_time.hour,
               local_time.minute, local_time.second);
 
-    if(history_size(history_list)==0) {
-        history_add(history_list, "M0VVA", "NONE", local_time);
-        history_add(history_list, "G4XIX", "NONE", local_time);
-        history_add(history_list, "M7TFT", "NONE", local_time);
-        history_add(history_list, "G0WCZ", "NONE", local_time);
-        history_add(history_list, "2E0MXA", "NONE", local_time);
-        history_add(history_list, "MM7RBK", "NONE", local_time);
-    }
+    // if(history_size(history_list)==0) {
+    //     history_add(history_list, "M0VVA", "NONE", local_time);
+    //     history_add(history_list, "G4XIX", "NONE", local_time);
+    //     history_add(history_list, "M7TFT", "NONE", local_time);
+    //     history_add(history_list, "G0WCZ", "NONE", local_time);
+    //     history_add(history_list, "2E0MXA", "NONE", local_time);
+    //     history_add(history_list, "MM7RBK", "NONE", local_time);
+    // }
 
 #endif
     // If the radio has no built-in battery, print input voltage
