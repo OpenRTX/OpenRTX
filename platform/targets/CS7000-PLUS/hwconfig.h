4f93c47b11c8fa640524458adbf7927bdee044b5
Silvano Seva <silseva@fastwebnet.it>
2024-11-02 19:27:54 +0100

    CS7000P: enabled NVM storage

--- platform/targets/CS7000-PLUS/hwconfig.h
+++ platform/targets/CS7000-PLUS/hwconfig.h
@@ -42,10 +42,9 @@ enum AdcChannels
 
 extern const struct Adc adc1;
 extern const struct spiCustomDevice spiSr;
-extern const struct spiCustomDevice flash_spi;
 extern const struct spiCustomDevice det_spi;
 extern const struct spiCustomDevice pll_spi;
-extern const struct spiCustomDevice c6000_spi;
+extern const struct spiDevice flash_spi;
 extern const struct gpioDev extGpio;
 extern const struct ak2365a detector;
 extern const struct sky73210 pll;
