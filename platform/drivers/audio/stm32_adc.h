1a15f793f3b80e20ffc55ad8a27ebfba871d4d53
Silvano Seva <silseva@fastwebnet.it>
2024-11-15 18:53:45 +0100

    Audio: STM32 ADC: extended driver to STM32H7 family

--- platform/drivers/audio/stm32_adc.h
+++ platform/drivers/audio/stm32_adc.h
@@ -1,8 +1,8 @@
 /***************************************************************************
- *   Copyright (C) 2023 by Federico Amedeo Izzo IU2NUO,                    *
- *                         Niccolò Izzo IU2KIN                             *
- *                         Frederik Saraci IU2NRO                          *
- *                         Silvano Seva IU2KWO                             *
+ *   Copyright (C) 2023 - 2024 by Federico Amedeo Izzo IU2NUO,             *
+ *                                Niccolò Izzo IU2KIN                      *
+ *                                Frederik Saraci IU2NRO                   *
+ *                                Silvano Seva IU2KWO                      *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -23,7 +23,6 @@
 
 #include <stdbool.h>
 #include <stdint.h>
-#include <stm32f4xx.h>
 #include <interfaces/audio.h>
 
 #ifdef __cplusplus
