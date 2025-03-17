fefbf83fccfa639871760f32dd9e4a3ffd1722bd
marco <49691247+marcoSchr@users.noreply.github.com>
2025-03-17 20:24:30 +0100

    RT-4D: Enable ADC driver for battery voltage

--- platform/targets/RT-4D/platform.c
+++ platform/targets/RT-4D/platform.c
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
+#include <drivers/ADC/adc_at32.h>
 #include <interfaces/platform.h>
 #include <peripherals/gpio.h>
 #include <hwconfig.h>
@@ -25,6 +26,10 @@ void platform_init()
     // Configure GPIOs
     gpio_setMode(GREEN_LED, OUTPUT);
     gpio_setMode(RED_LED, OUTPUT);
+
+    gpio_setMode(AIN_VBAT, ANALOG);
+
+    adcAt32_init(&adc1);
 }
 
 void platform_terminate()
@@ -36,7 +41,12 @@ void platform_terminate()
 
 uint16_t platform_getVbat()
 {
-    return 0;
+    /*
+  * Battery voltage is measured through an 1:7.4 voltage divider and
+  * adc1_getMeasurement returns a value in uV.
+  */
+    uint32_t vbat = adc_getVoltage(&adc1, ADC_VBAT_CH) * 74;
+    return vbat / 10000;
 }
 
 uint8_t platform_getMicLevel()
