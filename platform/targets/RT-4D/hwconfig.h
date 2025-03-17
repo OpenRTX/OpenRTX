3c41360a8c11d9070f00a203a1ba894f3c89feea
marco <49691247+marcoSchr@users.noreply.github.com>
2025-03-17 16:39:08 +0100

    RT-4D: Enable Screen Contrast

--- platform/targets/RT-4D/hwconfig.h
+++ platform/targets/RT-4D/hwconfig.h
@@ -16,6 +16,9 @@ extern const struct spiCustomDevice display_spi;
 #define CONFIG_SCREEN_WIDTH 128
 #define CONFIG_SCREEN_HEIGHT 64
 
+#define CONFIG_SCREEN_CONTRAST
+#define CONFIG_DEFAULT_CONTRAST 91
+
 /* Screen pixel format */
 #define CONFIG_PIX_FMT_BW
 
