891f27408e2adef71b9b995d040dbcbdef410c0a
Niccolò Izzo <izzo.niccolo@gmail.com>
2025-10-11 21:07:22 +0200

    Fix build for T-TWR REV2.1 target
    T-TWR Plus and T-TWR 2.1 need split devicetrees, given the complexity of
    the combination of OpenRTX and Zephyr build systems is just easier for
    now to create a separate target, and just re-use the source files where
    applicable.
    
    - Split ttwrplus and ttwr21 targets
    - Fix ttwr21 and ttwrplus dtsi include statements
    - Fix ttwr21 dts
    - Rename common source files :%s/ttwrplus/ttwr/g
    - Add meson target for ttwr21
    
    TG-553

---
+++ platform/targets/ttwr21/hwconfig.h
@@ -0,0 +1,41 @@
+/***************************************************************************
+ *   Copyright (C) 2023 - 2025 by Federico Amedeo Izzo IU2NUO,             *
+ *                                Niccolò Izzo IU2KIN                      *
+ *                                Silvano Seva IU2KWO                      *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
+ ***************************************************************************/
+
+#ifndef HWCONFIG_H
+#define HWCONFIG_H
+
+#include <zephyr/device.h>
+
+/*
+ * Display properties are encoded in the devicetree
+ */
+#define DISPLAY       DT_CHOSEN(zephyr_display)
+#define CONFIG_SCREEN_WIDTH  DT_PROP(DISPLAY, width)
+#define CONFIG_SCREEN_HEIGHT DT_PROP(DISPLAY, height)
+#define CONFIG_PIX_FMT_BW
+
+#define CONFIG_GPS
+#define CONFIG_NMEA_RBUF_SIZE 128
+
+#define CONFIG_BAT_LIPO
+#define CONFIG_BAT_NCELLS 1
+
+#define CONFIG_M17
+
+#endif /* HWCONFIG_H */
