575d285d8c0a58c5ace138b5f5a85e5a4e4d71d3
Ryan Tolboom <Ryan.Tolboom@njit.edu>
2026-02-03 13:29:55 -0500

    APRS: Demodulator: add AFSK1200 demodulator
    This is the initial commit of a demodulator for APRS packets. The
    Demodulator operates on chunks of baseband samples. It removes DC
    from the signal, discriminates between mark and space tones with
    FIR filters, and uses Slicer and Decoder to create frameData. It
    exclusively uses int16 arithmetic and is based on this prototype:
    https://github.com/rxt1077/aprs_prototype.
    
    Signed-off-by: Ryan Tolboom <Ryan.Tolboom@njit.edu>

--- scripts/clang_format.sh
+++ scripts/clang_format.sh
@@ -68,6 +68,7 @@ openrtx/include/protocols/APRS/constants.h
 openrtx/include/protocols/APRS/frame.h
 openrtx/include/protocols/APRS/Slicer.hpp
 openrtx/include/protocols/APRS/Decoder.hpp
+openrtx/include/protocols/APRS/Demodulator.hpp
 openrtx/src/core/dsp.cpp
 openrtx/src/core/nvmem_access.c
 openrtx/src/core/memory_profiling.cpp
@@ -75,6 +76,7 @@ openrtx/src/protocols/M17/Callsign.cpp
 openrtx/src/protocols/M17/M17FrameDecoder.cpp
 openrtx/src/protocols/APRS/Slicer.cpp
 openrtx/src/protocols/APRS/Decoder.cpp
+openrtx/src/protocols/APRS/Demodulator.cpp
 platform/drivers/ADC/ADC0_GDx.h
 platform/drivers/audio/file_source.h
 platform/drivers/audio/file_source.c
