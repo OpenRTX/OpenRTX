7c6dc4c0516c4b62e71e1898ffd4213935581187
Ryan Tolboom <Ryan.Tolboom@njit.edu>
2026-02-03 19:56:39 -0500

    APRS: packet: add packet structs and functions
    This is the initial commit of APRS code for creating APRS packets from
    HDLC frames. It also includes utility functions for manipulating the
    packet lists. It's written in C because the structs and functions will
    be used in the RTX and UI sections of the codebase.
    
    Signed-off-by: Ryan Tolboom <Ryan.Tolboom@njit.edu>

---
+++ openrtx/include/protocols/APRS/packet.h
@@ -0,0 +1,36 @@
+#ifndef APRS_PACKET_H
+#define APRS_PACKET_H
+
+#include <string.h>
+#include <stdlib.h>
+#include "interfaces/platform.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct address {
+    char addr[7]; /* include null termination */
+    uint8_t ssid : 4;
+    bool commandHeard;
+    struct address *next;
+} aprsAddress_t;
+
+typedef struct packet {
+    aprsAddress_t *addresses;
+    char *info;
+    datetime_t ts;
+    struct packet *prev;
+    struct packet *next;
+} aprsPacket_t;
+
+void aprsPktFree(aprsPacket_t *pkt);
+aprsPacket_t *aprsPktsInsert(aprsPacket_t *head, aprsPacket_t *pkt);
+aprsPacket_t *aprsPktDelete(aprsPacket_t *head, aprsPacket_t *pkt);
+aprsPacket_t *aprsPktFromFrame(uint8_t *frame, size_t frameSize);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* APRS_PACKET_H */
