79208b26ff41b6c1e9fdac59a4e1b95c5e9d3142
Silvano Seva <silseva@fastwebnet.it>
2026-02-24 21:03:34 +0100

    core: nvmem: update access API to new partition indexing
    Signed-off-by: Silvano Seva <silseva@fastwebnet.it>

--- openrtx/src/core/nvmem_access.c
+++ openrtx/src/core/nvmem_access.c
@@ -6,49 +6,8 @@
 
 #include "core/nvmem_access.h"
 #include "core/nvmem_device.h"
-#include <stdbool.h>
 #include <stdint.h>
 #include <errno.h>
-
-/**
- * \internal
- * Compute the absolute offset from the beginning of an NVM device, given the
- * device descriptor and the partition number. If the partition number is set
- * to -1, the offset is considered from the beginning of the device.
- * This function performs also a bound check to guarantee that the following
- * operation stays within the limits of the partition (if any).
- *
- * @param nvm: pointer to NVM descriptor.
- * @param part: partition number.
- * @param offset: pointer to the offset.
- * @param len: lenght of the read/write/erase operation.
- * @return a negative error code or zero.
- */
-static inline int getAbsOffset(const struct nvmDescriptor *nvm, const int part,
-                               uint32_t *offset, size_t len)
-{
-    const struct nvmPartition *np;
-    uint32_t offs = *offset;
-
-    // Offset is relative to a partition
-    if (part >= 0) {
-        if ((size_t)part >= nvm->nbPart)
-            return -EINVAL;
-
-        np = &nvm->partitions[part];
-
-        if ((offs + len) > np->size)
-            return -EINVAL;
-
-        offs += np->offset;
-    }
-
-    if ((offs + len) > nvm->size)
-        return -EINVAL;
-
-    *offset = offs;
-    return 0;
-}
 
 const struct nvmDescriptor *nvm_getDesc(const uint32_t index)
 {
@@ -78,43 +37,65 @@ int nvm_getPart(const uint32_t idx, const uint32_t part,
     return 0;
 }
 
-int nvm_read(const uint32_t dev, const int part, uint32_t offset, void *data,
-             size_t len)
+int nvm_read(const uint32_t idx, const uint32_t part, uint32_t offset,
+             void *data, size_t len)
 {
-    const struct nvmDescriptor *nvm = nvm_getDesc(dev);
-    if (nvm == NULL)
-        return -EINVAL;
+    const struct nvmDescriptor *nvm;
+    struct nvmPartition np;
+    int ret;
 
-    int ret = getAbsOffset(nvm, part, &offset, len);
+    ret = nvm_getPart(idx, part, &np);
     if (ret < 0)
         return ret;
+
+    // Accesses exceeding the nvm->size boundary are caugth by this check,
+    // provided that the partition table is well defined.
+    if ((offset + len) > np.size)
+        return -EINVAL;
+
+    // Index already validated by nvm_getPart
+    nvm = &nvmTab.areas[idx];
+    offset += np.offset;
 
     return nvm_devRead(nvm->dev, nvm->baseAddr + offset, data, len);
 }
 
-int nvm_write(const uint32_t dev, const int part, uint32_t offset,
+int nvm_write(const uint32_t idx, const uint32_t part, uint32_t offset,
               const void *data, size_t len)
 {
-    const struct nvmDescriptor *nvm = nvm_getDesc(dev);
-    if (nvm == NULL)
-        return -EINVAL;
+    const struct nvmDescriptor *nvm;
+    struct nvmPartition np;
+    int ret;
 
-    int ret = getAbsOffset(nvm, part, &offset, len);
+    ret = nvm_getPart(idx, part, &np);
     if (ret < 0)
         return ret;
+
+    if ((offset + len) > np.size)
+        return -EINVAL;
+
+    nvm = &nvmTab.areas[idx];
+    offset += np.offset;
 
     return nvm_devWrite(nvm->dev, nvm->baseAddr + offset, data, len);
 }
 
-int nvm_erase(const uint32_t dev, const int part, uint32_t offset, size_t size)
+int nvm_erase(const uint32_t idx, const uint32_t part, uint32_t offset,
+              size_t size)
 {
-    const struct nvmDescriptor *nvm = nvm_getDesc(dev);
-    if (nvm == NULL)
-        return -EINVAL;
+    const struct nvmDescriptor *nvm;
+    struct nvmPartition np;
+    int ret;
 
-    int ret = getAbsOffset(nvm, part, &offset, size);
+    ret = nvm_getPart(idx, part, &np);
     if (ret < 0)
         return ret;
+
+    if ((offset + size) > np.size)
+        return -EINVAL;
+
+    nvm = &nvmTab.areas[idx];
+    offset += np.offset;
 
     return nvm_devErase(nvm->dev, nvm->baseAddr + offset, size);
 }
