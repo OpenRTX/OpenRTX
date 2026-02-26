d27cc927b924c6e802d75f27174de090fb48c558
Ryan Turner <ryan@turnrye.com>
2026-02-26 17:58:56 -0600

    test: unit: clean up non-unit test dead code
    The unit test directory had a number of manual linux platform tests that aren't really unit tests. They aren't written with assertions and have no documented use. This change deletes these from the testing suite. In the future they could be added back in a different place, or automated tests in the form of true unit or integration tests could be added. But this clarifies that all suites in the unit test folder are actually automated unit tests, and only one unexectued test remains (related to code that is planned to be reintroduced in the future).
    
    Signed-off-by: Ryan Turner <ryan@turnrye.com>

--- scripts/clang_format.sh
+++ scripts/clang_format.sh
@@ -94,9 +94,7 @@ tests/unit/M17_demodulator.cpp
 tests/unit/M17_golay.cpp
 tests/unit/M17_rrc.cpp
 tests/unit/M17_viterbi.cpp
-tests/unit/play_sine.c
 tests/unit/ui_check_standby.cpp
-tests/unit/voice_prompts.c
 EOF
 )
 
