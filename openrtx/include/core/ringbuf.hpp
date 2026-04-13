7e9fe75114dc7b63ee32f2db28fe1fae91b74437
Silvano Seva <silseva@fastwebnet.it>
2026-04-13 20:05:12 +0200

    core: ringbuf: add nonblocking ring buffer class
    Added NbRingBuffer class, implementing a statically allocated circular
    buffer with only nonblocking operations. This allows to spare the memory
    cost (~96 bytes) of two pthread_cond_t variables when not needed.
    
    Signed-off-by: Silvano Seva <silseva@fastwebnet.it>

--- openrtx/include/core/ringbuf.hpp
+++ openrtx/include/core/ringbuf.hpp
@@ -1,6 +1,6 @@
 /*
  * SPDX-FileCopyrightText: Copyright 2020-2026 OpenRTX Contributors
- * 
+ *
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
@@ -13,22 +13,178 @@
 
 #include <pthread.h>
 #include <cstdint>
+#include <errno.h>
+
+/**
+ * Class implementing a statically allocated circular buffer with non-blocking
+ * push and pop functions.
+ */
+template <typename T, size_t N>
+class NbRingBuffer
+{
+public:
+    /**
+     * Constructor.
+     */
+    NbRingBuffer() : readPos(0), writePos(0), numElements(0)
+    {
+        pthread_mutex_init(&mutex, NULL);
+    }
+
+    /**
+     * Destructor.
+     */
+    ~NbRingBuffer()
+    {
+        pthread_mutex_destroy(&mutex);
+    }
+
+    /**
+     * Push an element to the buffer.
+     *
+     * @param elem: element to be pushed.
+     * @return zero on success, -EAGAIN if the queue is full and -EBUSY if the
+     * queue is kept by another thread.
+     */
+    int tryPush(const T &elem)
+    {
+        int ret = pthread_mutex_trylock(&mutex);
+        if (ret)
+            return ret;
+
+        if (full()) {
+            pthread_mutex_unlock(&mutex);
+            return -EAGAIN;
+        }
+
+        doPush(elem);
+        pthread_mutex_unlock(&mutex);
+
+        return 0;
+    }
+
+    /**
+     * Pop an element from the buffer.
+     *
+     * @param elem: place where to store the popped element.
+     * @return zero on success, -EAGAIN if the queue is empty and -EBUSY if the
+     * queue is kept by another thread.
+     */
+    int tryPop(T &elem)
+    {
+        int ret = pthread_mutex_trylock(&mutex);
+        if (ret)
+            return ret;
+
+        if (empty()) {
+            pthread_mutex_unlock(&mutex);
+            return -EAGAIN;
+        }
+
+        doPop(elem);
+        pthread_mutex_unlock(&mutex);
+
+        return 0;
+    }
+
+    /**
+     * Check if the buffer is empty.
+     * This function is not thread safe.
+     *
+     * @return true if the buffer is empty.
+     */
+    bool empty() const
+    {
+        return numElements == 0;
+    }
+
+    /**
+     * Check if the buffer is full.
+     * This function is not thread safe.
+     *
+     * @return true if the buffer is full.
+     */
+    bool full() const
+    {
+        return numElements >= N;
+    }
+
+    /**
+     * Get the number of elements currently stored in the buffer.
+     * This function is not thread safe.
+     *
+     * @return the number of elements currently present in the buffer.
+     */
+    size_t size() const
+    {
+        return numElements;
+    }
+
+    /**
+     * Reset the buffer to its empty state discarding all the elements stored,
+     * blocking function.
+
+     * Reset is "lazy", as it just sets read pointer, write pointer and
+     * number of elements to zero. No actual erasure of the stored elements is
+     * done until they are effectively overwritten by new push()es.
+     */
+    void reset()
+    {
+        pthread_mutex_lock(&mutex);
+        readPos = 0;
+        writePos = 0;
+        numElements = 0;
+        pthread_mutex_unlock(&mutex);
+    }
+
+protected:
+    /**
+     * Enqueueing helper function.
+     *
+     * @param elem: element to enqueue.
+     */
+    inline void doPush(const T &elem)
+    {
+        data[writePos++] = elem;
+        if (writePos == N)
+            writePos = 0;
+        numElements += 1;
+    }
+
+    /**
+     * Dequeueing helper function.
+     *
+     * @param elem: element to dequeue.
+     */
+    inline void doPop(T &elem)
+    {
+        elem = data[readPos++];
+        if (readPos == N)
+            readPos = 0;
+        numElements -= 1;
+    }
+
+    size_t readPos;        ///< Read pointer.
+    size_t writePos;       ///< Write pointer.
+    size_t numElements;    ///< Number of elements currently present.
+    T data[N];             ///< Data storage.
+
+    pthread_mutex_t mutex; ///< Mutex for concurrent access.
+};
 
 /**
  * Class implementing a statically allocated circular buffer with blocking and
  * non-blocking push and pop functions.
  */
-template < typename T, size_t N >
-class RingBuffer
+template <typename T, size_t N>
+class RingBuffer : public NbRingBuffer<T, N>
 {
 public:
-
     /**
      * Constructor.
      */
-    RingBuffer() : readPos(0), writePos(0), numElements(0)
+    RingBuffer()
     {
-        pthread_mutex_init(&mutex, NULL);
         pthread_cond_init(&not_empty, NULL);
         pthread_cond_init(&not_full, NULL);
     }
@@ -38,106 +194,50 @@ public:
      */
     ~RingBuffer()
     {
-        pthread_mutex_destroy(&mutex);
         pthread_cond_destroy(&not_empty);
         pthread_cond_destroy(&not_full);
     }
 
     /**
      * Push an element to the buffer.
+     * If the buffer is full the execution flow is blocked until at least one
+     * slot is available.
      *
      * @param elem: element to be pushed.
-     * @param blocking: if set to true, when the buffer is full this function
-     * blocks the execution flow until at least one empty slot is available.
-     * @return true if the element has been successfully pushed to the queue,
-     * false if the queue is empty.
      */
-    bool push(const T& elem, bool blocking)
+    void push(const T &elem)
     {
-        pthread_mutex_lock(&mutex);
+        pthread_mutex_lock(&this->mutex);
 
-        if((numElements >= N) && (blocking == false))
-        {
-            // No space available and non-blocking call: unlock mutex and return
-            pthread_mutex_unlock(&mutex);
-            return false;
-        }
+        while (this->numElements >= N)
+            pthread_cond_wait(&not_full, &this->mutex);
 
-        // The call is blocking: wait until there is some free space
-        while(numElements >= N)
-        {
-            pthread_cond_wait(&not_full, &mutex);
-        }
+        this->doPush(elem);
+        if (this->numElements == 1)
+            pthread_cond_signal(&not_empty);
 
-        // There is free space, push data into the queue
-        data[writePos] = elem;
-        writePos = (writePos + 1) % N;
-
-        // Signal that the queue is not empty
-        if(numElements == 0) pthread_cond_signal(&not_empty);
-        numElements += 1;
-
-        pthread_mutex_unlock(&mutex);
-        return true;
+        pthread_mutex_unlock(&this->mutex);
     }
 
     /**
      * Pop an element from the buffer.
+     * If the buffer is empty the execution flow is blocked until at least one
+     * element is available.
      *
      * @param elem: place where to store the popped element.
-     * @param blocking: if set to true, when the buffer is empty this function
-     * blocks the execution flow until at least one element is available.
-     * @return true if the element has been successfully popped from the queue,
-     * false if the queue is empty.
      */
-    bool pop(T& elem, bool blocking)
+    void pop(T &elem)
     {
-        pthread_mutex_lock(&mutex);
+        pthread_mutex_lock(&this->mutex);
 
-        if((numElements == 0) && (blocking == false))
-        {
-            // No elements present and non-blocking call: unlock mutex and return
-            pthread_mutex_unlock(&mutex);
-            return false;
-        }
+        while (this->numElements == 0)
+            pthread_cond_wait(&not_empty, &this->mutex);
 
-        // The call is blocking: wait until there is something into the queue
-        while(numElements == 0)
-        {
-            pthread_cond_wait(&not_empty, &mutex);
-        }
+        this->doPop(elem);
+        if (this->numElements == (N - 1))
+            pthread_cond_signal(&not_full);
 
-        // At least one element present pop one.
-        elem    = data[readPos];
-        readPos = (readPos + 1) % N;
-
-        // Signal that the queue is no more full
-        if(numElements >= N) pthread_cond_signal(&not_full);
-        numElements -= 1;
-
-        pthread_mutex_unlock(&mutex);
-
-        return true;
-    }
-
-    /**
-     * Check if the buffer is empty.
-     *
-     * @return true if the buffer is empty.
-     */
-    bool empty()
-    {
-        return numElements == 0;
-    }
-
-    /**
-     * Check if the buffer is full.
-     *
-     * @return true if the buffer is full.
-     */
-    bool full()
-    {
-        return numElements >= N;
+        pthread_mutex_unlock(&this->mutex);
     }
 
     /**
@@ -147,44 +247,25 @@ public:
      */
     void eraseElement()
     {
-        // Nothing to erase
-        if(numElements == 0) return;
+        pthread_mutex_lock(&this->mutex);
 
-        pthread_mutex_lock(&mutex);
+        if (this->numElements == 0) {
+            pthread_mutex_unlock(&this->mutex);
+            return;
+        }
 
-        // Chomp away one element just by advancing the read pointer.
-        readPos = (readPos + 1) % N;
+        this->readPos = (this->readPos + 1) % N;
+        this->numElements -= 1;
 
-        if(numElements >= N) pthread_cond_signal(&not_full);
-        numElements -= 1;
+        if (this->numElements == (N - 1))
+            pthread_cond_signal(&not_full);
 
-        pthread_mutex_unlock(&mutex);
-    }
-
-    /**
-     * Reset the buffer to its empty state discarding all the elements stored.
-     *
-     * Note: reset is "lazy", as it just sets read pointer, write pointer and
-     * number of elements to zero. No actual erasure of the stored elements is
-     * done until they are effectively overwritten by new push()es.
-     */
-    void reset()
-    {
-        readPos     = 0;
-        writePos    = 0;
-        numElements = 0;
+        pthread_mutex_unlock(&this->mutex);
     }
 
 private:
-
-    size_t readPos;      ///< Read pointer.
-    size_t writePos;     ///< Write pointer.
-    size_t numElements;  ///< Number of elements currently present.
-    T      data[N];      ///< Data storage.
-
-    pthread_mutex_t mutex;      ///< Mutex for concurrent access.
-    pthread_cond_t  not_empty;  ///< Queue not empty condition.
-    pthread_cond_t  not_full;   ///< Queue not full condition.
+    pthread_cond_t not_empty; ///< Queue not empty condition.
+    pthread_cond_t not_full;  ///< Queue not full condition.
 };
 
-#endif  // RINGBUF_H
+#endif // RINGBUF_H
