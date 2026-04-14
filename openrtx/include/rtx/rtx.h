50b93d2bfc1bb6ebad1ac07c65b51696837e78ae
Silvano Seva <silseva@fastwebnet.it>
2026-04-14 22:12:00 +0200

    rtx: apply clang-format to rtx.h and rtx.cpp
    Signed-off-by: Silvano Seva <silseva@fastwebnet.it>

--- openrtx/include/rtx/rtx.h
+++ openrtx/include/rtx/rtx.h
@@ -16,76 +16,70 @@
 extern "C" {
 #endif
 
-typedef struct
-{
+typedef struct {
     uint8_t opMode;         /**< Operating mode (FM, DMR, ...) */
 
-    uint8_t bandwidth : 2,  /**< Channel bandwidth             */
-            txDisable : 1,  /**< Disable TX operation          */
-            scan      : 1,  /**< Scan enabled                  */
-            opStatus  : 2,  /**< Operating status (OFF, ...)   */
-            _padding  : 2;  /**< Padding to 8 bits             */
+    uint8_t bandwidth : 2;  /**< Channel bandwidth             */
+    uint8_t txDisable : 1;  /**< Disable TX operation          */
+    uint8_t scan      : 1;  /**< Scan enabled                  */
+    uint8_t opStatus  : 2;  /**< Operating status (OFF, ...)   */
+    uint8_t           : 2;  /**< Padding to 8 bits             */
 
     freq_t rxFrequency;     /**< RX frequency, in Hz           */
     freq_t txFrequency;     /**< TX frequency, in Hz           */
 
     uint32_t txPower;       /**< TX power, in mW               */
-    uint8_t  sqlLevel;      /**< Squelch opening level         */
+    uint8_t sqlLevel;       /**< Squelch opening level         */
 
-    uint16_t rxToneEn : 1,  /**< RX CTC/DCS tone enable        */
-             rxTone   : 15; /**< RX CTC/DCS tone               */
+    uint16_t rxToneEn : 1;  /**< RX CTC/DCS tone enable        */
+    uint16_t rxTone   : 15; /**< RX CTC/DCS tone               */
 
-    uint16_t txToneEn : 1,  /**< TX CTC/DCS tone enable        */
-             txTone   : 15; /**< TX CTC/DCS tone               */
+    uint16_t txToneEn : 1;  /**< TX CTC/DCS tone enable        */
+    uint16_t txTone   : 15; /**< TX CTC/DCS tone               */
 
-    bool     toneEn;
+    bool toneEn;
 
-    uint8_t  can      : 4,  /**< M17 Channel Access Number     */
-             canRxEn  : 1,  /**< M17 Check CAN on RX           */
-             _unused  : 3;
+    uint8_t can     : 4; /**< M17 Channel Access Number     */
+    uint8_t canRxEn : 1; /**< M17 Check CAN on RX           */
+    uint8_t         : 3;
 
-    char     source_address[10];       /**< M17 call source address    */
-    char     destination_address[10];  /**< M17 call routing address   */
-    bool     invertRxPhase;            /**< M17 RX phase inversion     */
-    bool     lsfOk;                    /**  M17 LSF is valid           */
-    char     M17_dst[10];              /**  M17 LSF destination        */
-    char     M17_src[10];              /**  M17 LSF source             */
-    char     M17_link[10];             /**  M17 LSF traffic originator */
-    char     M17_refl[10];             /**  M17 LSF reflector module   */
-    char     M17_meta_text[53];        /**< M17 Meta Text              */
-}
-rtxStatus_t;
+    char source_address[10];      /**< M17 call source address    */
+    char destination_address[10]; /**< M17 call routing address   */
+    bool invertRxPhase;           /**< M17 RX phase inversion     */
+    bool lsfOk;                   /**  M17 LSF is valid           */
+    char M17_dst[10];             /**  M17 LSF destination        */
+    char M17_src[10];             /**  M17 LSF source             */
+    char M17_link[10];            /**  M17 LSF traffic originator */
+    char M17_refl[10];            /**  M17 LSF reflector module   */
+    char M17_meta_text[53];       /**< M17 Meta Text              */
+} rtxStatus_t;
 
 /**
  * \enum bandwidth Enumeration type defining the current rtx bandwidth.
  */
-enum bandwidth
-{
-    BW_12_5 = 0,    /**< 12.5kHz bandwidth */
-    BW_25   = 1     /**< 25kHz bandwidth   */
+enum bandwidth {
+    BW_12_5 = 0, /**< 12.5kHz bandwidth */
+    BW_25 = 1    /**< 25kHz bandwidth   */
 };
 
 /**
  * \enum opmode Enumeration type defining the current rtx operating mode.
  */
-enum opmode
-{
-    OPMODE_NONE = 0,        /**< No opMode selected */
-    OPMODE_FM   = 1,        /**< Analog FM          */
-    OPMODE_DMR  = 2,        /**< DMR                */
-    OPMODE_M17  = 3         /**< M17                */
+enum opmode {
+    OPMODE_NONE = 0, /**< No opMode selected */
+    OPMODE_FM = 1,   /**< Analog FM          */
+    OPMODE_DMR = 2,  /**< DMR                */
+    OPMODE_M17 = 3   /**< M17                */
 };
 
 /**
  * \enum opstatus Enumeration type defining the current rtx operating status.
  */
-enum opstatus
-{
-    OFF = 0,        /**< OFF          */
-    RX  = 1,        /**< Receiving    */
-    TX  = 2         /**< Transmitting */
+enum opstatus {
+    OFF = 0, /**< OFF          */
+    RX = 1,  /**< Receiving    */
+    TX = 2   /**< Transmitting */
 };
-
 
 /**
  * Initialise rtx stage.
