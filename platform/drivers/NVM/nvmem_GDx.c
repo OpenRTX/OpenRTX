69ccf76f7f0a3ceed7b15d227eb6f1894ed974de
Morgan Diepart <morgandiepart@ikmail.com>
2026-05-19 22:28:13 +0200

    [WIP] NVM: GDx: switch to new NVM APIs

--- platform/drivers/NVM/nvmem_GDx.c
+++ platform/drivers/NVM/nvmem_GDx.c
@@ -4,12 +4,12 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-#include <string.h>
-#include "wchar.h"
-#include "interfaces/delays.h"
+#include "core/cps.h"
+#include "core/settings.h"
+#include "core/struct_slicer.h"
+#include "core/vfo.h"
 #include "interfaces/nvmem.h"
 #include "core/nvmem_access.h"
-#include "core/nvmem_device.h"
 #include "calibration/calibInfo_GDx.h"
 #include "drivers/SPI/spi_bitbang.h"
 #include "hwconfig.h"
@@ -26,6 +26,39 @@ static const struct W25QxCfg eflashCfg =
 W25Qx_DEVICE_DEFINE(eflash, eflashCfg)
 AT24Cx_DEVICE_DEFINE(eeprom)
 
+static const struct nvmPartition eFlashPartitions[] =
+{
+#if defined(PLATFORM_GD77)
+    {
+        .offset = 0x8F000,
+        .size   = 0xE0
+    }
+#elif defined(PLATFORM_DM1801)
+    {
+        .offset = 0x6F000,
+        .size = 0xE0
+    }
+#else
+#error GDx calibration: platform not supported
+#endif
+};
+
+static const struct nvmPartition eepromPartitions[] =
+{
+    {
+        .offset = 0,
+        .size = 2048        // 2 kB, settings part A
+    },
+    {
+        .offset = 0x800,
+        .size = 2048        // 2 kB, settings part B
+    },
+    {
+        .offset = 0x1000,
+        .size = 1024        // 1kB, VFO storage
+    }
+};
+
 static const struct nvmDescriptor nvmDevices[] =
 {
     {
@@ -33,16 +66,16 @@ static const struct nvmDescriptor nvmDevices[] =
         .dev        = &eflash,
         .baseAddr   = 0x00000000,
         .size       = 0x100000,     // 1 MB,  8 Mbit
-        .nbPart     = 0,
-        .partitions = NULL
+        .nbPart     = ARRAY_SIZE(eFlashPartitions),
+        .partitions = eFlashPartitions
     },
     {
         .name       = "EEPROM",
         .dev        = &eeprom,
         .baseAddr   = 0x00000000,
         .size       = 0x10000,      // 64 kB, 512 kbit
-        .nbPart     = 0,
-        .partitions = NULL
+        .nbPart     = ARRAY_SIZE(eepromPartitions),
+        .partitions = eepromPartitions
     }
 };
 
@@ -51,15 +84,11 @@ const struct nvmTable nvmTab = {
     .nbAreas = ARRAY_SIZE(nvmDevices),
 };
 
-#if defined(PLATFORM_GD77)
-static const uint32_t UHF_CAL_BASE = 0x8F000;
-static const uint32_t VHF_CAL_BASE = 0x8F070;
-#elif defined(PLATFORM_DM1801)
-static const uint32_t UHF_CAL_BASE = 0x6F000;
-static const uint32_t VHF_CAL_BASE = 0x6F070;
-#else
-#warning GDx calibration: platform not supported
-#endif
+static const uint32_t UHF_CAL_BASE = 0x00;
+static const uint32_t VHF_CAL_BASE = 0x70;
+
+static struct vfo_storage vfo_storage;
+static struct settings_storage settings_storage;
 
 /**
  * \internal Utility function for loading band-specific calibration data into
@@ -70,42 +99,42 @@ static const uint32_t VHF_CAL_BASE = 0x6F070;
  */
 static void _loadBandCalData(uint32_t baseAddr, bandCalData_t *cal)
 {
-    nvm_devRead(&eflash, baseAddr + 0x08, &(cal->modBias),            2);
-    nvm_devRead(&eflash, baseAddr + 0x0A, &(cal->mod2Offset),         1);
-    nvm_devRead(&eflash, baseAddr + 0x3F, cal->analogSqlThresh,       8);
-    nvm_devRead(&eflash, baseAddr + 0x47, &(cal->noise1_HighTsh_Wb),  1);
-    nvm_devRead(&eflash, baseAddr + 0x48, &(cal->noise1_LowTsh_Wb),   1);
-    nvm_devRead(&eflash, baseAddr + 0x49, &(cal->noise2_HighTsh_Wb),  1);
-    nvm_devRead(&eflash, baseAddr + 0x4A, &(cal->noise2_LowTsh_Wb),   1);
-    nvm_devRead(&eflash, baseAddr + 0x4B, &(cal->rssi_HighTsh_Wb),    1);
-    nvm_devRead(&eflash, baseAddr + 0x4C, &(cal->rssi_LowTsh_Wb),     1);
-    nvm_devRead(&eflash, baseAddr + 0x4D, &(cal->noise1_HighTsh_Nb),  1);
-    nvm_devRead(&eflash, baseAddr + 0x4E, &(cal->noise1_LowTsh_Nb),   1);
-    nvm_devRead(&eflash, baseAddr + 0x4F, &(cal->noise2_HighTsh_Nb),  1);
-    nvm_devRead(&eflash, baseAddr + 0x50, &(cal->noise2_LowTsh_Nb),   1);
-    nvm_devRead(&eflash, baseAddr + 0x51, &(cal->rssi_HighTsh_Nb),    1);
-    nvm_devRead(&eflash, baseAddr + 0x52, &(cal->rssi_LowTsh_Nb),     1);
-    nvm_devRead(&eflash, baseAddr + 0x53, &(cal->RSSILowerThreshold), 1);
-    nvm_devRead(&eflash, baseAddr + 0x54, &(cal->RSSIUpperThreshold), 1);
-    nvm_devRead(&eflash, baseAddr + 0x55, cal->mod1Amplitude,         8);
-    nvm_devRead(&eflash, baseAddr + 0x5D, &(cal->digAudioGain),       1);
-    nvm_devRead(&eflash, baseAddr + 0x5E, &(cal->txDev_DTMF),         1);
-    nvm_devRead(&eflash, baseAddr + 0x5F, &(cal->txDev_tone),         1);
-    nvm_devRead(&eflash, baseAddr + 0x60, &(cal->txDev_CTCSS_wb),     1);
-    nvm_devRead(&eflash, baseAddr + 0x61, &(cal->txDev_CTCSS_nb),     1);
-    nvm_devRead(&eflash, baseAddr + 0x62, &(cal->txDev_DCS_wb),       1);
-    nvm_devRead(&eflash, baseAddr + 0x63, &(cal->txDev_DCS_nb),       1);
-    nvm_devRead(&eflash, baseAddr + 0x64, &(cal->PA_drv),             1);
-    nvm_devRead(&eflash, baseAddr + 0x65, &(cal->PGA_gain),           1);
-    nvm_devRead(&eflash, baseAddr + 0x66, &(cal->analogMicGain),      1);
-    nvm_devRead(&eflash, baseAddr + 0x67, &(cal->rxAGCgain),          1);
-    nvm_devRead(&eflash, baseAddr + 0x68, &(cal->mixGainWideband),    2);
-    nvm_devRead(&eflash, baseAddr + 0x6A, &(cal->mixGainNarrowband),  2);
-    nvm_devRead(&eflash, baseAddr + 0x6C, &(cal->rxDacGain),          1);
-    nvm_devRead(&eflash, baseAddr + 0x6D, &(cal->rxVoiceGain),        1);
+    nvm_read(0, 1, baseAddr + 0x08, &(cal->modBias),            2);
+    nvm_read(0, 1, baseAddr + 0x0A, &(cal->mod2Offset),         1);
+    nvm_read(0, 1, baseAddr + 0x3F, cal->analogSqlThresh,       8);
+    nvm_read(0, 1, baseAddr + 0x47, &(cal->noise1_HighTsh_Wb),  1);
+    nvm_read(0, 1, baseAddr + 0x48, &(cal->noise1_LowTsh_Wb),   1);
+    nvm_read(0, 1, baseAddr + 0x49, &(cal->noise2_HighTsh_Wb),  1);
+    nvm_read(0, 1, baseAddr + 0x4A, &(cal->noise2_LowTsh_Wb),   1);
+    nvm_read(0, 1, baseAddr + 0x4B, &(cal->rssi_HighTsh_Wb),    1);
+    nvm_read(0, 1, baseAddr + 0x4C, &(cal->rssi_LowTsh_Wb),     1);
+    nvm_read(0, 1, baseAddr + 0x4D, &(cal->noise1_HighTsh_Nb),  1);
+    nvm_read(0, 1, baseAddr + 0x4E, &(cal->noise1_LowTsh_Nb),   1);
+    nvm_read(0, 1, baseAddr + 0x4F, &(cal->noise2_HighTsh_Nb),  1);
+    nvm_read(0, 1, baseAddr + 0x50, &(cal->noise2_LowTsh_Nb),   1);
+    nvm_read(0, 1, baseAddr + 0x51, &(cal->rssi_HighTsh_Nb),    1);
+    nvm_read(0, 1, baseAddr + 0x52, &(cal->rssi_LowTsh_Nb),     1);
+    nvm_read(0, 1, baseAddr + 0x53, &(cal->RSSILowerThreshold), 1);
+    nvm_read(0, 1, baseAddr + 0x54, &(cal->RSSIUpperThreshold), 1);
+    nvm_read(0, 1, baseAddr + 0x55, cal->mod1Amplitude,         8);
+    nvm_read(0, 1, baseAddr + 0x5D, &(cal->digAudioGain),       1);
+    nvm_read(0, 1, baseAddr + 0x5E, &(cal->txDev_DTMF),         1);
+    nvm_read(0, 1, baseAddr + 0x5F, &(cal->txDev_tone),         1);
+    nvm_read(0, 1, baseAddr + 0x60, &(cal->txDev_CTCSS_wb),     1);
+    nvm_read(0, 1, baseAddr + 0x61, &(cal->txDev_CTCSS_nb),     1);
+    nvm_read(0, 1, baseAddr + 0x62, &(cal->txDev_DCS_wb),       1);
+    nvm_read(0, 1, baseAddr + 0x63, &(cal->txDev_DCS_nb),       1);
+    nvm_read(0, 1, baseAddr + 0x64, &(cal->PA_drv),             1);
+    nvm_read(0, 1, baseAddr + 0x65, &(cal->PGA_gain),           1);
+    nvm_read(0, 1, baseAddr + 0x66, &(cal->analogMicGain),      1);
+    nvm_read(0, 1, baseAddr + 0x67, &(cal->rxAGCgain),          1);
+    nvm_read(0, 1, baseAddr + 0x68, &(cal->mixGainWideband),    2);
+    nvm_read(0, 1, baseAddr + 0x6A, &(cal->mixGainNarrowband),  2);
+    nvm_read(0, 1, baseAddr + 0x6C, &(cal->rxDacGain),          1);
+    nvm_read(0, 1, baseAddr + 0x6D, &(cal->rxVoiceGain),        1);
 
     uint8_t txPwr[32] = {0};
-    nvm_devRead(&eflash, baseAddr + 0x0B, txPwr, 32);
+    nvm_read(0, 1, baseAddr + 0x0B, txPwr, 32);
 
     for(uint8_t i = 0; i < 16; i++)
     {
@@ -120,6 +149,8 @@ void nvm_init()
     spiBitbang_init(&nvm_spi);
     W25Qx_init(&eflash);
     AT24Cx_init();
+    vfo_initStorage(&vfo_storage, 1, 3, sizeof(channel_t) + vfo_storageOverhead(NULL), CPS_VERSION_NUMBER);
+    settings_initStorage(&settings_storage, 1, 1, 2);
 }
 
 void nvm_terminate()
